0777b96f28315e9bc51d2a702a8971a96b43cd9d
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2024-12-01 13:05:05 +0800

    修改涤净青金矿为重力方块

--- src/main/java/net/mcreator/oneiricconcept/block/VarunadaLazuriteOreBlock.java
+++ src/main/java/net/mcreator/oneiricconcept/block/VarunadaLazuriteOreBlock.java
@@ -5,11 +5,19 @@ import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.core.BlockPos;
 
-public class VarunadaLazuriteOreBlock extends Block {
+import com.mojang.serialization.MapCodec;
+
+public class VarunadaLazuriteOreBlock extends FallingBlock {
+	public static final MapCodec<VarunadaLazuriteOreBlock> CODEC = simpleCodec(properties -> new VarunadaLazuriteOreBlock());
+
+	public MapCodec<VarunadaLazuriteOreBlock> codec() {
+		return CODEC;
+	}
+
 	public VarunadaLazuriteOreBlock() {
 		super(BlockBehaviour.Properties.of().mapColor(MapColor.SAND).sound(SoundType.SAND).strength(4f, 10f).requiresCorrectToolForDrops());
 	}
