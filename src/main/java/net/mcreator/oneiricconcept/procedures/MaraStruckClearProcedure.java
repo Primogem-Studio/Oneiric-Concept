2b14efbc2295726fdce4af2e4d70182a24056062
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2024-12-20 19:51:03 +0800

    微调，添加魔阴身清除流程

---
+++ src/main/java/net/mcreator/oneiricconcept/procedures/MaraStruckClearProcedure.java
@@ -0,0 +1,42 @@
+package net.mcreator.oneiricconcept.procedures;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+
+import java.util.List;
+import java.util.Comparator;
+
+public class MaraStruckClearProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity ent, double model) {
+		if (ent == null)
+			return;
+		if (0 == model) {
+			{
+				Entity _ent = ent;
+				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "effect clear @s primogemcraft:fengrao");
+				}
+			}
+		} else if (1 == model) {
+			{
+				final Vec3 _center = new Vec3(x, y, z);
+				List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(13 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+				for (Entity entityiterator : _entfound) {
+					{
+						Entity _ent = entityiterator;
+						if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+							_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+									_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "effect clear @s primogemcraft:fengrao");
+						}
+					}
+				}
+			}
+		}
+	}
+}
