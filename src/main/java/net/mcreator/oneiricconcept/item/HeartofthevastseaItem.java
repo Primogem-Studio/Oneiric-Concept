af0d3492830ff1e0a6a1c8fb0bc455c10aff797b
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-01-06 19:55:50 +0800

    调整博洋之心buff和添加配方

--- src/main/java/net/mcreator/oneiricconcept/item/HeartofthevastseaItem.java
+++ src/main/java/net/mcreator/oneiricconcept/item/HeartofthevastseaItem.java
@@ -1,12 +1,16 @@
 
 package net.mcreator.oneiricconcept.item;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.InteractionResult;
 
 import net.mcreator.oneiricconcept.procedures.WaterplaceProcedure;
+import net.mcreator.oneiricconcept.procedures.CuriovastseaProcedure;
 
 public class HeartofthevastseaItem extends Item {
 	public HeartofthevastseaItem() {
@@ -18,5 +22,11 @@ public class HeartofthevastseaItem extends Item {
 		super.useOn(context);
 		WaterplaceProcedure.execute(context.getLevel(), context.getClickedPos().getX(), context.getClickedPos().getY(), context.getClickedPos().getZ(), context.getClickedFace());
 		return InteractionResult.SUCCESS;
+	}
+
+	@Override
+	public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
+		super.inventoryTick(itemstack, world, entity, slot, selected);
+		CuriovastseaProcedure.execute(entity);
 	}
 }
