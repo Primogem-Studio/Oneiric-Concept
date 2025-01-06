a931c18bbcf721acdf4800adc45ead7c1a5a261a
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-01-06 17:34:08 +0800

    调整撒币枪，适配全局游戏规则

--- src/main/java/net/mcreator/oneiricconcept/procedures/MoraLaunchProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/MoraLaunchProcedure.java
@@ -13,7 +13,6 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.BlockPos;
 
 public class MoraLaunchProcedure {
@@ -22,21 +21,18 @@ public class MoraLaunchProcedure {
 			return;
 		ItemStack MainHand = ItemStack.EMPTY;
 		ItemStack SecondaryHand = ItemStack.EMPTY;
-		double nu = 0;
 		SecondaryHand = (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).copy();
 		if (SecondaryHand.is(ItemTags.create(ResourceLocation.parse("oneiricconcept:magazine1")))) {
 			MoraDamageProcedure.execute(world, entity, 1, 5);
-			nu = 20;
 		} else if (SecondaryHand.is(ItemTags.create(ResourceLocation.parse("oneiricconcept:magazine2")))) {
 			MoraDamageProcedure.execute(world, entity, 2, 5);
-			nu = 20;
 		} else if (SecondaryHand.is(ItemTags.create(ResourceLocation.parse("oneiricconcept:magazine3")))) {
 			MoraDamageProcedure.execute(world, entity, 4, 16);
 			if (world instanceof Level _level) {
 				if (!_level.isClientSide()) {
-					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.trident.thunder")), SoundSource.PLAYERS, 2, 1);
+					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.trident.thunder")), SoundSource.PLAYERS, 1, 1);
 				} else {
-					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.trident.thunder")), SoundSource.PLAYERS, 2, 1, false);
+					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.trident.thunder")), SoundSource.PLAYERS, 1, 1, false);
 				}
 			}
 			if (world instanceof ServerLevel _level) {
@@ -45,11 +41,6 @@ public class MoraLaunchProcedure {
 				entityToSpawn.setVisualOnly(true);
 				_level.addFreshEntity(entityToSpawn);
 			}
-			nu = 0;
 		}
-		if (world instanceof ServerLevel _level)
-			_level.sendParticles(ParticleTypes.LARGE_SMOKE, x, (y + 1.3), z, 10, 1.3, 1.3, 1.3, 0.3);
-		if (world instanceof ServerLevel _level)
-			_level.sendParticles(ParticleTypes.TRIAL_SPAWNER_DETECTED_PLAYER, x, (y + 1.3), z, (int) nu, 0.1, 0.1, 0.1, 0.1);
 	}
 }
