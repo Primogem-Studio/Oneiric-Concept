badb3af1939c14d74deb0e6c49298a27b43d0ac2
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-01-18 02:03:04 +0800

    添加撒币枪介绍

---
+++ src/main/java/net/mcreator/oneiricconcept/procedures/MagazineTipProcedure.java
@@ -0,0 +1,9 @@
+package net.mcreator.oneiricconcept.procedures;
+
+import net.minecraft.network.chat.Component;
+
+public class MagazineTipProcedure {
+	public static String execute() {
+		return Component.translatable("translation.oneiricconcept.magazine").getString();
+	}
+}
