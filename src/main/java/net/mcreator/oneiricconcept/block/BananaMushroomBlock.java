a9adf1834fe7cb7dc132981d83944d6748f50db9
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-01-26 18:14:04 +0800

    萌萌菌种植

--- src/main/java/net/mcreator/oneiricconcept/block/BananaMushroomBlock.java
+++ src/main/java/net/mcreator/oneiricconcept/block/BananaMushroomBlock.java
@@ -8,32 +8,33 @@ import net.neoforged.api.distmarker.Dist;
 
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.grower.TreeGrower;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.FlowerBlock;
-import net.minecraft.world.level.block.BonemealableBlock;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.SaplingBlock;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.util.RandomSource;
-import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
-import net.mcreator.oneiricconcept.procedures.BananaMushroomboneProcedure;
-import net.mcreator.oneiricconcept.procedures.BananaMushroomNagadusProcedure;
-
+import java.util.Optional;
 import java.util.List;
 
-public class BananaMushroomBlock extends FlowerBlock implements BonemealableBlock {
+public class BananaMushroomBlock extends SaplingBlock {
+	public static final TreeGrower TREE_GROWER = new TreeGrower("banana_mushroom", 0.2f, Optional.empty(), Optional.empty(), Optional.of(getFeatureKey("oneiricconcept:b_2anana_mushroom")), Optional.of(getFeatureKey("oneiricconcept:mm_mushroom")),
+			Optional.empty(), Optional.empty());
+
 	public BananaMushroomBlock() {
-		super(MobEffects.LUCK, 100, BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW).sound(SoundType.GRASS).instabreak().lightLevel(s -> 3).noCollission().offsetType(BlockBehaviour.OffsetType.NONE).pushReaction(PushReaction.DESTROY));
+		super(TREE_GROWER,
+				BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW).randomTicks().sound(SoundType.GRASS).instabreak().lightLevel(s -> 3).noCollission().offsetType(BlockBehaviour.OffsetType.NONE).pushReaction(PushReaction.DESTROY));
 	}
 
 	@Override
@@ -58,18 +59,7 @@ public class BananaMushroomBlock extends FlowerBlock implements BonemealableBloc
 		return 60;
 	}
 
-	@Override
-	public boolean isValidBonemealTarget(LevelReader worldIn, BlockPos pos, BlockState blockstate) {
-		return true;
-	}
-
-	@Override
-	public boolean isBonemealSuccess(Level world, RandomSource random, BlockPos pos, BlockState blockstate) {
-		return BananaMushroomboneProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
-	}
-
-	@Override
-	public void performBonemeal(ServerLevel world, RandomSource random, BlockPos pos, BlockState blockstate) {
-		BananaMushroomNagadusProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
+	private static ResourceKey<ConfiguredFeature<?, ?>> getFeatureKey(String feature) {
+		return ResourceKey.create(Registries.CONFIGURED_FEATURE, ResourceLocation.parse(feature));
 	}
 }
