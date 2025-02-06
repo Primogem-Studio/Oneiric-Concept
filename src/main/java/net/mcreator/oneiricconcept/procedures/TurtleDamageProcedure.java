3db3d14e30a1745c5b75aa30aaadbbdb9216376f
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-02-06 17:18:29 +0800

    水晶龟调整2
    调整水晶重生时间20分和缩壳流程

--- src/main/java/net/mcreator/oneiricconcept/procedures/TurtleDamageProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/TurtleDamageProcedure.java
@@ -1,16 +1,19 @@
 package net.mcreator.oneiricconcept.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.BlockPos;
 
 import net.mcreator.oneiricconcept.init.OneiricconceptModGameRules;
 import net.mcreator.oneiricconcept.entity.CrystalTurtleEntity;
@@ -21,8 +24,12 @@ public class TurtleDamageProcedure {
 			return false;
 		ItemStack items = ItemStack.EMPTY;
 		double Times = 0;
+		double timeste = 0;
+		boolean Damagetyplgc = false;
 		Times = entity instanceof CrystalTurtleEntity _datEntI ? _datEntI.getEntityData().get(CrystalTurtleEntity.DATA_ShrinkingShellTime) : 0;
-		if (!(entity instanceof CrystalTurtleEntity _datEntL1 && _datEntL1.getEntityData().get(CrystalTurtleEntity.DATA_IsCrystallized))) {
+		timeste = Math.max(Math.min(1200, Times + 600), 600);
+		Damagetyplgc = damagesource.is(DamageTypes.FELL_OUT_OF_WORLD) || damagesource.is(DamageTypes.GENERIC_KILL);
+		if (!(entity instanceof CrystalTurtleEntity _datEntL3 && _datEntL3.getEntityData().get(CrystalTurtleEntity.DATA_IsCrystallized))) {
 			items = new ItemStack(BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:ysjsj")));
 			items.setCount((int) RandomintProcedure.execute(5, 1));
 			if (world instanceof ServerLevel _level) {
@@ -30,19 +37,30 @@ public class TurtleDamageProcedure {
 				entityToSpawn.setPickUpDelay(10);
 				_level.addFreshEntity(entityToSpawn);
 			}
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.amethyst_cluster.break")), SoundSource.NEUTRAL, 3, 1);
+				} else {
+					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.amethyst_cluster.break")), SoundSource.NEUTRAL, 3, 1, false);
+				}
+			}
 			if (entity instanceof CrystalTurtleEntity _datEntSetI)
-				_datEntSetI.getEntityData().set(CrystalTurtleEntity.DATA_crystal, 1200);
+				_datEntSetI.getEntityData().set(CrystalTurtleEntity.DATA_crystal, 24000);
 			if (entity instanceof CrystalTurtleEntity _datEntSetL)
 				_datEntSetL.getEntityData().set(CrystalTurtleEntity.DATA_IsCrystallized, true);
+			if (entity instanceof CrystalTurtleEntity _datEntSetI)
+				_datEntSetI.getEntityData().set(CrystalTurtleEntity.DATA_ShrinkingShellTime, (int) timeste);
+			if (entity instanceof CrystalTurtleEntity _datEntSetL)
+				_datEntSetL.getEntityData().set(CrystalTurtleEntity.DATA_IsShrinking, true);
 			if (world.getLevelData().getGameRules().getBoolean(OneiricconceptModGameRules.OCDEBUG)) {
 				if (sourceentity instanceof Player _player && !_player.level().isClientSide())
 					_player.displayClientMessage(Component.literal(("\u6C34\u6676\u65F6\u95F4\uFF1A\u00A7b" + (entity instanceof CrystalTurtleEntity _datEntI ? _datEntI.getEntityData().get(CrystalTurtleEntity.DATA_crystal) : 0)
 							+ "\u00A7r\u7F29\u58F3\u65F6\u95F4\uFF1A\u00A79" + (entity instanceof CrystalTurtleEntity _datEntI ? _datEntI.getEntityData().get(CrystalTurtleEntity.DATA_ShrinkingShellTime) : 0))), false);
 			}
-			return Times <= 0;
+			return Times <= 0 || Damagetyplgc;
 		}
 		if (entity instanceof CrystalTurtleEntity _datEntSetI)
-			_datEntSetI.getEntityData().set(CrystalTurtleEntity.DATA_ShrinkingShellTime, (int) Math.max(Math.min(1200, Times + 600), 600));
+			_datEntSetI.getEntityData().set(CrystalTurtleEntity.DATA_ShrinkingShellTime, (int) timeste);
 		if (entity instanceof CrystalTurtleEntity _datEntSetL)
 			_datEntSetL.getEntityData().set(CrystalTurtleEntity.DATA_IsShrinking, true);
 		if (world.getLevelData().getGameRules().getBoolean(OneiricconceptModGameRules.OCDEBUG)) {
@@ -50,6 +68,6 @@ public class TurtleDamageProcedure {
 				_player.displayClientMessage(Component.literal(("\u6C34\u6676\u65F6\u95F4\uFF1A\u00A7b" + (entity instanceof CrystalTurtleEntity _datEntI ? _datEntI.getEntityData().get(CrystalTurtleEntity.DATA_crystal) : 0)
 						+ "\u00A7r\u7F29\u58F3\u65F6\u95F4\uFF1A\u00A79" + (entity instanceof CrystalTurtleEntity _datEntI ? _datEntI.getEntityData().get(CrystalTurtleEntity.DATA_ShrinkingShellTime) : 0))), false);
 		}
-		return !ShrinkingShellSuoKeProcedure.execute(entity) || damagesource.is(DamageTypes.GENERIC_KILL) || damagesource.is(DamageTypes.FELL_OUT_OF_WORLD);
+		return Damagetyplgc || !ShrinkingShellSuoKeProcedure.execute(entity);
 	}
 }
