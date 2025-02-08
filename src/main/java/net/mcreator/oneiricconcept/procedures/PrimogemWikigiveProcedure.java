efaf95a876c639ba5f03c10f553005568c54acd5
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-02-09 00:47:15 +0800

    调整本地化，修复悬空引用

--- src/main/java/net/mcreator/oneiricconcept/procedures/PrimogemWikigiveProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/PrimogemWikigiveProcedure.java
@@ -1,5 +1,7 @@
 package net.mcreator.oneiricconcept.procedures;
 
+import net.neoforged.fml.ModList;
+
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
@@ -11,15 +13,17 @@ public class PrimogemWikigiveProcedure {
 	public static void execute(Entity entity, ItemStack itemstack) {
 		if (entity == null)
 			return;
-		if (entity instanceof Player _player) {
-			ItemStack _stktoremove = itemstack;
-			_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
-		}
-		{
-			Entity _ent = entity;
-			if (!_ent.level().isClientSide() && _ent.getServer() != null) {
-				_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-						_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "/give @s patchouli:guide_book[patchouli:book=\"oneiricconcept:primogem_wiki\"]");
+		if (ModList.get().isLoaded("patchouli")) {
+			if (entity instanceof Player _player) {
+				ItemStack _stktoremove = itemstack;
+				_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+			}
+			{
+				Entity _ent = entity;
+				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "/give @s patchouli:guide_book[patchouli:book=\"oneiricconcept:primogem_wiki\"]");
+				}
 			}
 		}
 	}
