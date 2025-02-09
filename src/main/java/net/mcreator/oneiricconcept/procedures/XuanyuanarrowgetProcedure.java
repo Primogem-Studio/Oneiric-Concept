33c7e7ddebe858304939399f8f796090ce322686
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-02-10 04:01:54 +0800

    修复进度声音

--- src/main/java/net/mcreator/oneiricconcept/procedures/XuanyuanarrowgetProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/XuanyuanarrowgetProcedure.java
@@ -16,8 +16,7 @@ public class XuanyuanarrowgetProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		if (!(entity instanceof ServerPlayer _plr0 && _plr0.level() instanceof ServerLevel && _plr0.getAdvancements().getOrStartProgress(_plr0.server.getAdvancements().get(ResourceLocation.parse("oneiricconcept:shootingthesun"))).isDone()
-				&& world.isClientSide())) {
+		if (!(entity instanceof ServerPlayer _plr0 && _plr0.level() instanceof ServerLevel && _plr0.getAdvancements().getOrStartProgress(_plr0.server.getAdvancements().get(ResourceLocation.parse("oneiricconcept:shootingthesun"))).isDone())) {
 			if (entity instanceof ServerPlayer _player) {
 				AdvancementHolder _adv = _player.server.getAdvancements().get(ResourceLocation.parse("oneiricconcept:shootingthesun"));
 				if (_adv != null) {
@@ -28,11 +27,13 @@ public class XuanyuanarrowgetProcedure {
 					}
 				}
 			}
-			if (world instanceof Level _level) {
-				if (!_level.isClientSide()) {
-					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("oneiricconcept:houyi")), SoundSource.NEUTRAL, 1, 1);
-				} else {
-					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("oneiricconcept:houyi")), SoundSource.NEUTRAL, 1, 1, false);
+			if (world.isClientSide()) {
+				if (world instanceof Level _level) {
+					if (!_level.isClientSide()) {
+						_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("oneiricconcept:houyi")), SoundSource.NEUTRAL, 1, 1);
+					} else {
+						_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("oneiricconcept:houyi")), SoundSource.NEUTRAL, 1, 1, false);
+					}
 				}
 			}
 		}
