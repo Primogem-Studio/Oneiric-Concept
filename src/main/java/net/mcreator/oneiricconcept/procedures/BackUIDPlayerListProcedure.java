565990a81e23b9aa622679462ed44b9dbf587457
hackermdch <58121864+hackermdch@users.noreply.github.com>
2025-03-12 12:09:01 +0800

    Optimized code

--- src/main/java/net/mcreator/oneiricconcept/procedures/BackUIDPlayerListProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/BackUIDPlayerListProcedure.java
@@ -1,18 +1,12 @@
 package net.mcreator.oneiricconcept.procedures;
 
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.entity.Entity;
 
-import java.util.List;
 import java.util.ArrayList;
 
 public class BackUIDPlayerListProcedure {
-	public static ArrayList execute(LevelAccessor world) {
-		List<Object> playerlist = new ArrayList<>();
-		for (Entity entityiterator : new ArrayList<>(world.players())) {
-			playerlist.add(entityiterator);
-		}
-        ArrayList<Object> result = new ArrayList<>(playerlist);
-        return result;
-	}
+    public static ArrayList<Player> execute(LevelAccessor world) {
+        return new ArrayList<>(world.players());
+    }
 }
