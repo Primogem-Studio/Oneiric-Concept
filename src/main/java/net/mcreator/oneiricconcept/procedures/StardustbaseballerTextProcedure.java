a5826cc117ef635b38ace10c1887ba02dc20c5c1
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-03-28 16:44:26 +0800

    星尘球棒描述基础

--- src/main/java/net/mcreator/oneiricconcept/procedures/StardustbaseballerTextProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/StardustbaseballerTextProcedure.java
@@ -8,17 +8,18 @@ import net.minecraft.core.component.DataComponents;
 public class StardustbaseballerTextProcedure {
 	public static String execute(ItemStack itemstack) {
 		double a = 0;
-		String nametext = "";
+		String itemname = "";
+		String tran = "";
 		a = itemstack.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("jing_lian");
-		nametext = "stardust_baseballer";
-		nametext = "translation.oneiricconcept." + nametext + "_";
-		return PgcwuqiProcedure.execute(itemstack,
-				(("\u00A76\u00A7l\u00A7n" + Component.translatable("translation.oneiricconcept.passive").getString()) + "\n"
-						+ ("\u00A7e" + Component.translatable((nametext + "1")).getString() + "\u00A7b" + new java.text.DecimalFormat("##.##").format(a) + "\u00A7e" + Component.translatable((nametext + "2")).getString()))
-						+ ""
-						+ (("\u00A76\u00A7l\u00A7n" + Component.translatable("translation.oneiricconcept.rightclick").getString()) + "\n"
-								+ ("\u00A7e" + Component.translatable((nametext + "1")).getString() + "\u00A7b" + new java.text.DecimalFormat("##.##").format(a) + "\u00A7e" + Component.translatable((nametext + "2")).getString()))
-						+ (("\u00A76\u00A7l\u00A7n" + Component.translatable("translation.oneiricconcept.leftclick").getString()) + "\n"
-								+ ("\u00A7e" + Component.translatable((nametext + "1")).getString() + "\u00A7b" + new java.text.DecimalFormat("##.##").format(a) + "\u00A7e" + Component.translatable((nametext + "2")).getString())));
+		tran = "translation.oneiricconcept.";
+		itemname = "translation.oneiricconcept.";
+		itemname = tran + "" + itemname + "_";
+		return PgcwuqiProcedure.execute(itemstack, (("\u00A76\u00A7l\u00A7n" + Component.translatable((tran + "passive")).getString() + "\n") + ""
+				+ ("\u00A7e" + (itemname + "1") + "\u00A7b" + new java.text.DecimalFormat("##.##").format(a) + "\u00A7e" + (itemname + "2") + "\u00A7b" + new java.text.DecimalFormat("##.##").format(a) + "\n" + "\u00A7e" + (itemname + "3")))
+				+ ""
+				+ (("\u00A76\u00A7l\u00A7n" + Component.translatable((tran + "rightclick")).getString() + "\n") + ""
+						+ ("\u00A7e" + (itemname + "4") + "\u00A7b" + new java.text.DecimalFormat("##.##").format(a) + "\u00A7e" + (itemname + "5") + "\u00A7b" + new java.text.DecimalFormat("##.##").format(a) + "\n" + "\u00A7e" + (tran + "second")))
+				+ (("\u00A76\u00A7l\u00A7n" + Component.translatable((tran + "leftclick")).getString() + "\n") + "" + ("\u00A7e" + (itemname + "6") + "\u00A7b" + new java.text.DecimalFormat("##.##").format(a) + "\u00A7e" + (itemname + "7")
+						+ "\u00A7b" + new java.text.DecimalFormat("##.##").format(a) + "\n" + "\u00A7e" + (tran + "second"))));
 	}
 }
