f368ecc735436f8445320df0ff2e63ab85a39b1c
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-03-30 19:54:34 +0800

    发电流程调整

--- src/main/java/net/mcreator/oneiricconcept/procedures/CrystalgeneratorListProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/CrystalgeneratorListProcedure.java
@@ -1,16 +1,40 @@
 package net.mcreator.oneiricconcept.procedures;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 
+import java.util.Locale;
 import java.util.List;
 import java.util.ArrayList;
 
 public class CrystalgeneratorListProcedure {
-public static void execute(
-) {
-ItemStack crystal = ItemStack.EMPTY;double crystal_power = 0;String pgc = "";boolean iscrystals = false;List<Object> list16000 = new ArrayList<>();List<Object> list64000 = new ArrayList<>();List<Object> list160000 = new ArrayList<>();List<Object> list1600000 = new ArrayList<>();
-pgc = "primogemcraft:";list16000.add(BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc+"zuishengduanpian")).toLowerCase(java.util.Locale.ENGLISH))));list16000.add(CUSTOM:PgcPg);list16000.add(BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc+"disui")).toLowerCase(java.util.Locale.ENGLISH))));list16000.add(BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc+"yhjsl")).toLowerCase(java.util.Locale.ENGLISH))));list16000.add(BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc+"jlliang")).toLowerCase(java.util.Locale.ENGLISH))));list16000.add(BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc+"yuanshi")).toLowerCase(java.util.Locale.ENGLISH))));list64000.add(CUSTOM:PhlogistonCrystal);list64000.add(BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc+"zuishengkuai")).toLowerCase(java.util.Locale.ENGLISH))));list64000.add(CUSTOM:HonkaiSliver);list64000.add(BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc+"laoshi")).toLowerCase(java.util.Locale.ENGLISH))));list64000.add(BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc+"chuangshijiejing")).toLowerCase(java.util.Locale.ENGLISH))));list160000.add(CUSTOM:Elect1);list160000.add(BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc+"yhjs")).toLowerCase(java.util.Locale.ENGLISH))));list1600000.add(CUSTOM:HonkaiShard);list1600000.add(BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc+"zuishengzijing")).toLowerCase(java.util.Locale.ENGLISH))));
-}
+	public static double execute(ItemStack itemstack) {
+		ItemStack crystal = ItemStack.EMPTY;
+		double crystal_power = 0;
+		String pgc = "";
+		boolean iscrystals = false;
+		pgc = "primogemcraft:";
+		crystal = itemstack;
+		List<Object> list16000 = new ArrayList<>(List.of(BuiltInRegistries.ITEM.get(ResourceLocation.parse(pgc + "zuishengduanpian").toLowerCase(Locale.ENGLISH)), CUSTOM.PgcPg,
+				BuiltInRegistries.ITEM.get(ResourceLocation.parse(pgc + "disui").toLowerCase(Locale.ENGLISH)), BuiltInRegistries.ITEM.get(ResourceLocation.parse(pgc + "yhjsl").toLowerCase(Locale.ENGLISH)),
+				BuiltInRegistries.ITEM.get(ResourceLocation.parse(pgc + "jlliang").toLowerCase(Locale.ENGLISH)), BuiltInRegistries.ITEM.get(ResourceLocation.parse(pgc + "yuanshi").toLowerCase(Locale.ENGLISH))));
+		List<Object> list64000 = new ArrayList<>(List.of(CUSTOM.PhlogistonCrystal, BuiltInRegistries.ITEM.get(ResourceLocation.parse(pgc + "zuishengkuai").toLowerCase(Locale.ENGLISH)), CUSTOM.HonkaiSliver,
+				BuiltInRegistries.ITEM.get(ResourceLocation.parse(pgc + "laoshi").toLowerCase(Locale.ENGLISH)), BuiltInRegistries.ITEM.get(ResourceLocation.parse(pgc + "chuangshijiejing").toLowerCase(Locale.ENGLISH))));
+		List<Object> list160000 = new ArrayList<>(List.of(CUSTOM.Elect1, BuiltInRegistries.ITEM.get(ResourceLocation.parse(pgc + "yhjs").toLowerCase(Locale.ENGLISH))));
+		List<Object> list1600000 = new ArrayList<>(List.of(CUSTOM.HonkaiShard, BuiltInRegistries.ITEM.get(ResourceLocation.parse(pgc + "zuishengzijing").toLowerCase(Locale.ENGLISH))));
+		if (list16000.contains(crystal)) {
+			crystal_power = 16000;
+		} else if (list64000.contains(crystal)) {
+			crystal_power = 64000;
+		} else if (list160000.contains(crystal)) {
+			crystal_power = 160000;
+		} else if (list1600000.contains(crystal)) {
+			crystal_power = 1600000;
+		} else if (crystal.is(ItemTags.create(ResourceLocation.parse("c::crystals")))) {
+			crystal_power = 1600;
+		}
+		return crystal_power;
+	}
 }
--- src/main/java/net/mcreator/oneiricconcept/procedures/CrystalgeneratoritemProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/CrystalgeneratoritemProcedure.java
@@ -1,20 +1,16 @@
 package net.mcreator.oneiricconcept.procedures;
 
+import net.neoforged.neoforge.items.IItemHandlerModifiable;
 import net.neoforged.neoforge.items.IItemHandler;
 import net.neoforged.neoforge.common.extensions.ILevelExtension;
 import net.neoforged.neoforge.capabilities.Capabilities;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.BlockPos;
 
-import net.mcreator.oneiricconcept.init.OneiricconceptModItems;
 import net.mcreator.oneiricconcept.init.OneiricconceptModGameRules;
-import net.mcreator.oneiricconcept.init.OneiricconceptModBlocks;
 
 public class CrystalgeneratoritemProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
@@ -30,35 +26,21 @@ public class CrystalgeneratoritemProcedure {
 					world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("\u5728\u7B2C" + index0 + "\u683C\u7684\u7269\u54C1\uFF1A" + crystal)), false);
 			}
 			if (!(crystal == ItemStack.EMPTY)) {
+				if (world instanceof ILevelExtension _ext && _ext.getCapability(Capabilities.ItemHandler.BLOCK, BlockPos.containing(x, y, z), null) instanceof IItemHandlerModifiable _itemHandlerModifiable) {
+					int _slotid = (int) index0;
+					ItemStack _stk = _itemHandlerModifiable.getStackInSlot(_slotid).copy();
+					_stk.shrink(1);
+					_itemHandlerModifiable.setStackInSlot(_slotid, _stk);
+				}
 				break;
 			}
 		}
-		if (OneiricconceptModBlocks.PRIMOGEM.get().asItem() == crystal.getItem() || BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc + "zuishengduanpian")).toLowerCase(java.util.Locale.ENGLISH))) == crystal.getItem()
-				|| BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc + "disui")).toLowerCase(java.util.Locale.ENGLISH))) == crystal.getItem()
-				|| BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc + "yhjsl")).toLowerCase(java.util.Locale.ENGLISH))) == crystal.getItem()
-				|| BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc + "jlliang")).toLowerCase(java.util.Locale.ENGLISH))) == crystal.getItem()
-				|| BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc + "yuanshi")).toLowerCase(java.util.Locale.ENGLISH))) == crystal.getItem()) {
-			crystal_power = 16000;
-		} else if (OneiricconceptModItems.PHLOGISTON_CRYSTAL.get() == crystal.getItem() || BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc + "zuishengkuai")).toLowerCase(java.util.Locale.ENGLISH))) == crystal.getItem()
-				|| BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc + "laoshi")).toLowerCase(java.util.Locale.ENGLISH))) == crystal.getItem() || OneiricconceptModItems.HONKAI_SLIVER.get() == crystal.getItem()
-				|| BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc + "chuangshijiejing")).toLowerCase(java.util.Locale.ENGLISH))) == crystal.getItem()) {
-			crystal_power = 64000;
-		} else if (OneiricconceptModBlocks.ELECTRO_METAL.get().asItem() == crystal.getItem() || BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc + "yhjs")).toLowerCase(java.util.Locale.ENGLISH))) == crystal.getItem()) {
-			crystal_power = 160000;
-		} else if (OneiricconceptModItems.HONKAI_SHARD.get() == crystal.getItem() || BuiltInRegistries.ITEM.get(ResourceLocation.parse(((pgc + "zuishengzijing")).toLowerCase(java.util.Locale.ENGLISH))) == crystal.getItem()) {
-			crystal_power = 1600000;
-		} else if (crystal.is(ItemTags.create(ResourceLocation.parse("c::crystals")))) {
-			crystal_power = 1600;
-		} else {
-			iscrystals = true;
+		crystal_power = CrystalgeneratorListProcedure.execute(crystal);
+		if (world.getLevelData().getGameRules().getBoolean(OneiricconceptModGameRules.OCDEBUG)) {
+			if (!world.isClientSide() && world.getServer() != null)
+				world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("\u5411\u53D1\u7535\u6D41\u7A0B\u4F20\u8F93\u7535\u529B\uFF1A" + crystal_power + "\u7269\u54C1\uFF1A" + crystal)), false);
 		}
-		if (!iscrystals) {
-			if (world.getLevelData().getGameRules().getBoolean(OneiricconceptModGameRules.OCDEBUG)) {
-				if (!world.isClientSide() && world.getServer() != null)
-					world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("\u5411\u53D1\u7535\u6D41\u7A0B\u4F20\u8F93\u7535\u529B\uFF1A" + crystal_power + "\u7269\u54C1\uFF1A" + crystal)), false);
-			}
-			CrystalgeneratorpowerProcedure.execute(world, x, y, z, crystal_power);
-		}
+		CrystalgeneratorpowerProcedure.execute(world, x, y, z, crystal_power);
 	}
 
 	private static ItemStack itemFromBlockInventory(LevelAccessor world, BlockPos pos, int slot) {
