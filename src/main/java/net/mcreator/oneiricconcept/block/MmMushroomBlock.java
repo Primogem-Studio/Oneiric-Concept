b4f241d477d51c38fbed5414b98f131049cef572
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-04-02 21:34:28 +0800

    调整萌萌菌生成

--- src/main/java/net/mcreator/oneiricconcept/block/MmMushroomBlock.java
+++ src/main/java/net/mcreator/oneiricconcept/block/MmMushroomBlock.java
@@ -14,21 +14,21 @@ import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.DoublePlantBlock;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
+import net.mcreator.oneiricconcept.procedures.SupportdownProcedure;
 import net.mcreator.oneiricconcept.procedures.MmhitProcedure;
 
 import java.util.List;
@@ -55,21 +55,27 @@ public class MmMushroomBlock extends DoublePlantBlock {
 		return 60;
 	}
 
-	private boolean canPlantTypeSurvive(BlockState state, LevelReader world, BlockPos pos) {
-		return state.is(BlockTags.DIRT) || state.getBlock() == Blocks.FARMLAND;
+	@Override
+	public boolean mayPlaceOn(BlockState groundState, BlockGetter worldIn, BlockPos pos) {
+		boolean additionalCondition = true;
+		if (worldIn instanceof LevelAccessor world) {
+			int x = pos.getX();
+			int y = pos.getY() + 1;
+			int z = pos.getZ();
+			BlockState blockstate = world.getBlockState(pos.above());
+			additionalCondition = SupportdownProcedure.execute(world, x, y, z);
+		}
+		return additionalCondition;
 	}
 
 	@Override
-	public boolean canSurvive(BlockState blockstate, LevelReader world, BlockPos pos) {
-		BlockPos posbelow = pos.below();
-		BlockState statebelow = world.getBlockState(posbelow);
-		if (blockstate.getValue(HALF) != DoubleBlockHalf.UPPER) {
-			if (blockstate.getBlock() == this)
-				return this.canPlantTypeSurvive(statebelow, world, posbelow);
-			return this.mayPlaceOn(statebelow, world, posbelow);
-		} else {
-			return statebelow.is(this) && statebelow.getValue(HALF) == DoubleBlockHalf.LOWER;
-		}
+	public boolean canSurvive(BlockState blockstate, LevelReader worldIn, BlockPos pos) {
+		BlockPos blockpos = pos.below();
+		BlockState groundState = worldIn.getBlockState(blockpos);
+		if (blockstate.getValue(HALF) == DoubleBlockHalf.UPPER)
+			return groundState.is(this) && groundState.getValue(HALF) == DoubleBlockHalf.LOWER;
+		else
+			return this.mayPlaceOn(groundState, worldIn, blockpos);
 	}
 
 	@Override
