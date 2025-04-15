17496d01ea031754442c4b1737dfddfbae261f8b
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-04-16 01:03:17 +0800

    Revert "优化重子生成"
    This reverts commit 1fb6c68f05bc3142ef6b79a4e6afbc3d062c319e.

--- src/main/java/net/mcreator/oneiricconcept/procedures/BaryonPlaceProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/BaryonPlaceProcedure.java
@@ -5,10 +5,8 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
-import net.mcreator.oneiricconcept.init.OneiricconceptModGameRules;
-
 public class BaryonPlaceProcedure {
 	public static boolean execute(LevelAccessor world, double x, double y, double z) {
-		return (world.getBlockState(BlockPos.containing(x, y - 1, z))).is(BlockTags.create(ResourceLocation.parse("oneiricconcept:topblock"))) && world.getLevelData().getGameRules().getBoolean(OneiricconceptModGameRules.ZZSC);
+		return (world.getBlockState(BlockPos.containing(x, y - 1, z))).is(BlockTags.create(ResourceLocation.parse("oneiricconcept:topblock")));
 	}
 }
