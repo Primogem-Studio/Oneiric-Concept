d609c54fb43d2c91c42ff759629324f425e5f5d0
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-07-09 23:04:36 +0800

    反物质炸弹特效

--- src/main/java/net/mcreator/oneiricconcept/procedures/BlockClearProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/BlockClearProcedure.java
@@ -1,11 +1,17 @@
 package net.mcreator.oneiricconcept.procedures;
 
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.tags.TagKey;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.core.BlockPos;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 
 import net.mcreator.oneiricconcept.OneiricconceptMod;
 
@@ -16,8 +22,12 @@ public class BlockClearProcedure {
 		for (int index0 = 0; index0 < 16; index0++) {
 			sz = 0;
 			for (int index1 = 0; index1 < 16; index1++) {
-				if (-1 != world.getBlockState(BlockPos.containing(x + sx, y, z + sz)).getDestroySpeed(world, BlockPos.containing(x + sx, y, z + sz))) {
+				if (-1 != world.getBlockState(BlockPos.containing(x + sx, y, z + sz)).getDestroySpeed(world, BlockPos.containing(x + sx, y, z + sz)) && !((world.getBlockState(BlockPos.containing(x + sx, y, z + sz))).getBlock() == Blocks.AIR)) {
 					world.setBlock(BlockPos.containing(x + sx, y, z + sz), Blocks.AIR.defaultBlockState(), 3);
+					if (world instanceof ServerLevel _level)
+						_level.getServer().getCommands().performPrefixedCommand(
+								new CommandSourceStack(CommandSource.NULL, new Vec3((x + sx), y, (z + sz)), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+								"particle minecraft:flash ~ ~ ~ 0 0 0 1 1 force");
 				}
 				sz = sz + 1;
 			}
