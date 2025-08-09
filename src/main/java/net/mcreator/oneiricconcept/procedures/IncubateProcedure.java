ee47d61a69406a80ca01a2bf1c0f1e3c612dec04
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-08-10 01:52:29 +0800

    鸟窝在打开后才孵化

--- src/main/java/net/mcreator/oneiricconcept/procedures/IncubateProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/IncubateProcedure.java
@@ -7,6 +7,7 @@ import net.neoforged.neoforge.capabilities.Capabilities;
 
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
@@ -24,33 +25,42 @@ public class IncubateProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		ItemStack egg = ItemStack.EMPTY;
 		boolean stop = false;
-		for (int index0 = 0; index0 < 16; index0++) {
-			egg = (itemFromBlockInventory(world, BlockPos.containing(x, y, z), (int) index0).copy());
-			{
-				final Vec3 _center = new Vec3(x, y, z);
-				for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(5 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
-					if (RandomProcedure.execute(world, entityiterator instanceof Chicken && !(entityiterator instanceof LivingEntity _livEnt2 && _livEnt2.isBaby()) ? 0.8 : 0.3) && egg.getItem() == Items.EGG) {
-						if (world instanceof ILevelExtension _ext && _ext.getCapability(Capabilities.ItemHandler.BLOCK, BlockPos.containing(x, y, z), null) instanceof IItemHandlerModifiable _itemHandlerModifiable) {
-							int _slotid = (int) index0;
-							ItemStack _stk = _itemHandlerModifiable.getStackInSlot(_slotid).copy();
-							_stk.shrink(1);
-							_itemHandlerModifiable.setStackInSlot(_slotid, _stk);
-						}
-						if (world instanceof ServerLevel _level) {
-							Entity entityToSpawn = EntityType.CHICKEN.spawn(_level, BlockPos.containing(x, y, z), MobSpawnType.MOB_SUMMONED);
-							if (entityToSpawn != null) {
-								entityToSpawn.setYRot(world.getRandom().nextFloat() * 360F);
+		if (getBlockNBTNumber(world, BlockPos.containing(x, y, z), "mode") == 1) {
+			for (int index0 = 0; index0 < 16; index0++) {
+				egg = (itemFromBlockInventory(world, BlockPos.containing(x, y, z), (int) index0).copy());
+				{
+					final Vec3 _center = new Vec3(x, y, z);
+					for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(5 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
+						if (RandomProcedure.execute(world, entityiterator instanceof Chicken && !(entityiterator instanceof LivingEntity _livEnt3 && _livEnt3.isBaby()) ? 0.8 : 0.3) && egg.getItem() == Items.EGG) {
+							if (world instanceof ILevelExtension _ext && _ext.getCapability(Capabilities.ItemHandler.BLOCK, BlockPos.containing(x, y, z), null) instanceof IItemHandlerModifiable _itemHandlerModifiable) {
+								int _slotid = (int) index0;
+								ItemStack _stk = _itemHandlerModifiable.getStackInSlot(_slotid).copy();
+								_stk.shrink(1);
+								_itemHandlerModifiable.setStackInSlot(_slotid, _stk);
 							}
+							if (world instanceof ServerLevel _level) {
+								Entity entityToSpawn = EntityType.CHICKEN.spawn(_level, BlockPos.containing(x, y, z), MobSpawnType.MOB_SUMMONED);
+								if (entityToSpawn != null) {
+									entityToSpawn.setYRot(world.getRandom().nextFloat() * 360F);
+								}
+							}
+							stop = true;
+							break;
 						}
-						stop = true;
-						break;
 					}
 				}
-			}
-			if (stop) {
-				break;
+				if (stop) {
+					break;
+				}
 			}
 		}
+	}
+
+	private static double getBlockNBTNumber(LevelAccessor world, BlockPos pos, String tag) {
+		BlockEntity blockEntity = world.getBlockEntity(pos);
+		if (blockEntity != null)
+			return blockEntity.getPersistentData().getDouble(tag);
+		return -1;
 	}
 
 	private static ItemStack itemFromBlockInventory(LevelAccessor world, BlockPos pos, int slot) {
--- src/main/java/net/mcreator/oneiricconcept/procedures/State1Procedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/State1Procedure.java
@@ -2,7 +2,9 @@ package net.mcreator.oneiricconcept.procedures;
 
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
 import net.minecraft.core.BlockPos;
 
 public class State1Procedure {
@@ -13,6 +15,15 @@ public class State1Procedure {
 			BlockState _bs = world.getBlockState(_pos);
 			if (_bs.getBlock().getStateDefinition().getProperty("blockstate") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
 				world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
+		}
+		if (!world.isClientSide()) {
+			BlockPos _bp = BlockPos.containing(x, y, z);
+			BlockEntity _blockEntity = world.getBlockEntity(_bp);
+			BlockState _bs = world.getBlockState(_bp);
+			if (_blockEntity != null)
+				_blockEntity.getPersistentData().putDouble("mode", 1);
+			if (world instanceof Level _level)
+				_level.sendBlockUpdated(_bp, _bs, _bs, 3);
 		}
 	}
 }
