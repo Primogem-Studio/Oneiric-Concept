c8c04bc21c3fa4aa8f126c1364d22295d1f3f891
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-08-14 01:10:40 +0800

    修改建木木头不燃烧

--- src/main/java/net/mcreator/oneiricconcept/block/AmbrosialArborWoodBlock.java
+++ src/main/java/net/mcreator/oneiricconcept/block/AmbrosialArborWoodBlock.java
@@ -54,11 +54,6 @@ public class AmbrosialArborWoodBlock extends Block {
 	}
 
 	@Override
-	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
-		return 20;
-	}
-
-	@Override
 	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
 		return 500;
 	}
