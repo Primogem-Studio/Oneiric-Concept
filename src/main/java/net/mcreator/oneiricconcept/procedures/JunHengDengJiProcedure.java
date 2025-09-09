86beaa29a61f0d99df7b9323383b09dd305fc9d8
Pepper MR <zheshixbde@outlook.com>
2025-09-09 22:42:52 +0800

    推

--- src/main/java/net/mcreator/oneiricconcept/procedures/JunHengDengJiProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/JunHengDengJiProcedure.java
@@ -4,11 +4,12 @@ import net.mcreator.ceshi.network.PrimogemcraftModVariables;
 import net.minecraft.world.entity.Entity;
 
 public class JunHengDengJiProcedure {
-	public static double execute(Entity entity) {
-		if (entity == null)
-			return 0;
-		Entity entity0 = null;
-		entity0 = entity;
-		return entity.getData(PrimogemcraftModVariables.PLAYER_VARIABLES).jun_heng/1000;
-	}
+    public static double execute(Entity entity) {
+        if (entity == null)
+            return 0;
+        Entity entity0 = null;
+        entity0 = entity;
+        var a = entity.getData(PrimogemcraftModVariables.PLAYER_VARIABLES).jun_heng / 10;
+        return a > 3 ? 3 : a;
+    }
 }
