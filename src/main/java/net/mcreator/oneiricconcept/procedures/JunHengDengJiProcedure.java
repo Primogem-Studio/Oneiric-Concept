a4ccb8bbf81eea566dff2e64a70f8743d6154515
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-09-09 22:10:45 +0800

    均衡等级模板

---
+++ src/main/java/net/mcreator/oneiricconcept/procedures/JunHengDengJiProcedure.java
@@ -0,0 +1,13 @@
+package net.mcreator.oneiricconcept.procedures;
+
+import net.minecraft.world.entity.Entity;
+
+public class JunHengDengJiProcedure {
+	public static double execute(Entity entity) {
+		if (entity == null)
+			return 0;
+		Entity entity0 = null;
+		entity0 = entity;
+		return 0;
+	}
+}
