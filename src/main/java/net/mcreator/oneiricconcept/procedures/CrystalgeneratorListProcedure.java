4bf1512667588b7965cf1b4e264912ac3ea98b1b
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2025-10-04 01:19:42 +0800

    优化晶体发电机物品判断

--- src/main/java/net/mcreator/oneiricconcept/procedures/CrystalgeneratorListProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/CrystalgeneratorListProcedure.java
@@ -3,10 +3,6 @@ package net.mcreator.oneiricconcept.procedures;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.core.registries.BuiltInRegistries;
-
-import net.mcreator.oneiricconcept.init.OneiricconceptModItems;
-import net.mcreator.oneiricconcept.init.OneiricconceptModBlocks;
 
 public class CrystalgeneratorListProcedure {
 	public static double execute(ItemStack itemstack) {
@@ -15,18 +11,13 @@ public class CrystalgeneratorListProcedure {
 		ItemStack crystal = ItemStack.EMPTY;
 		ItemStack listitem = ItemStack.EMPTY;
 		crystal = itemstack;
-		if (OneiricconceptModBlocks.PRIMOGEM.get().asItem() == crystal.getItem() || BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:yuanshi")) == crystal.getItem()
-				|| BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:disui")) == crystal.getItem() || BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:zuishengduanpian")) == crystal.getItem()
-				|| BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:yhjsl")) == crystal.getItem() || BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:jlliang")) == crystal.getItem()) {
+		if (crystal.is(ItemTags.create(ResourceLocation.parse("oneiricconcept:crystals1")))) {
 			crystal_power = 16000;
-		} else if (OneiricconceptModItems.HONKAI_SLIVER.get() == crystal.getItem() || OneiricconceptModItems.PHLOGISTON_CRYSTAL.get() == crystal.getItem()
-				|| BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:zuishengkuai")) == crystal.getItem() || BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:laoshi")) == crystal.getItem()
-				|| BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:chuangshijiejing")) == crystal.getItem() || OneiricconceptModItems.ONEIRIC_SHARD.get() == crystal.getItem()) {
+		} else if (crystal.is(ItemTags.create(ResourceLocation.parse("oneiricconcept:crystals2")))) {
 			crystal_power = 64000;
-		} else if (BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:yhjs")) == crystal.getItem() || OneiricconceptModBlocks.ELECTRO_METAL.get().asItem() == crystal.getItem()
-				|| OneiricconceptModItems.CULLINAN.get() == crystal.getItem()) {
+		} else if (crystal.is(ItemTags.create(ResourceLocation.parse("oneiricconcept:crystals3")))) {
 			crystal_power = 160000;
-		} else if (OneiricconceptModItems.HONKAI_SHARD.get() == crystal.getItem() || BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:zuishengzijing")) == crystal.getItem()) {
+		} else if (crystal.is(ItemTags.create(ResourceLocation.parse("oneiricconcept:crystals4")))) {
 			crystal_power = 1600000;
 		} else if (crystal.is(ItemTags.create(ResourceLocation.parse("c:crystals")))) {
 			crystal_power = 160;
