9bd57b3c308ba13bf47e91772e6dbd9e1770e594
Stella <1130668892@qq.com>
2025-10-27 17:24:19 +0800

    修改跳过之杖
    增加消耗的饥饿值、饱和度；修复异常生效扣除数值问题；统一专票nbt便于堆叠；缩短冷却时间

--- src/main/java/net/mcreator/oneiricconcept/procedures/SkipdamageProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/SkipdamageProcedure.java
@@ -14,45 +14,47 @@ import net.mcreator.oneiricconcept.init.OneiricconceptModGameRules;
 import net.mcreator.oneiricconcept.OneiricconceptMod;
 
 public class SkipdamageProcedure {
-	public static void execute(LevelAccessor world, Entity entity, ItemStack itemstack, double indexx) {
+	public static void execute(LevelAccessor world, Entity entity, ItemStack itemstack, boolean db, double indexx) {
 		if (entity == null)
 			return;
-		ItemStack zpitem = ItemStack.EMPTY;
-		double ktime = 0;
-		if (0 < (entity instanceof Player _plr ? _plr.getFoodData().getSaturationLevel() : 0)) {
-			if (entity instanceof Player _player)
-				_player.getFoodData().setSaturation((float) ((entity instanceof Player _plr ? _plr.getFoodData().getSaturationLevel() : 0) - 0.5));
-		} else if (0 < (entity instanceof Player _plr ? _plr.getFoodData().getFoodLevel() : 0)) {
-			if (entity instanceof Player _player)
-				_player.causeFoodExhaustion((float) 0.5);
-		} else if (entity.isAlive()) {
-			entity.invulnerableTime = 0;
-			if (entity instanceof LivingEntity _entity) {
-				DamageSource _dmgsource = new DamageSource(world.holderOrThrow(DamageTypes.GENERIC));
-				_entity.hurt(new DamageSource(_dmgsource.typeHolder(), _dmgsource.getEntity(), _dmgsource.getDirectEntity()) {
-					@Override
-					public Component getLocalizedDeathMessage(LivingEntity _msgEntity) {
-						String _translatekey = "death.attack.skip";
-						if (this.getEntity() == null && this.getDirectEntity() == null) {
-							return _msgEntity.getKillCredit() != null
-									? Component.translatable(_translatekey + ".player", _msgEntity.getDisplayName(), _msgEntity.getKillCredit().getDisplayName())
-									: Component.translatable(_translatekey, _msgEntity.getDisplayName());
-						} else {
-							Component _component = this.getEntity() == null ? this.getDirectEntity().getDisplayName() : this.getEntity().getDisplayName();
-							ItemStack _itemstack = ItemStack.EMPTY;
-							if (this.getEntity() instanceof LivingEntity _livingentity)
-								_itemstack = _livingentity.getMainHandItem();
-							return !_itemstack.isEmpty() && _itemstack.get(DataComponents.CUSTOM_NAME) != null
-									? Component.translatable(_translatekey + ".item", _msgEntity.getDisplayName(), _component, _itemstack.getDisplayName())
-									: Component.translatable(_translatekey, _msgEntity.getDisplayName(), _component);
-						}
-					}
-				}, (world.getLevelData().getGameRules().getInt(OneiricconceptModGameRules.OC_HEALTHMULTIPLIER)));
-			}
+		if (world.getLevelData().getGameRules().getBoolean(OneiricconceptModGameRules.OCDEBUG) && db) {
+			if (!world.isClientSide() && world.getServer() != null)
+				world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("\u63A5\u6536\u5230\u8DF3\u8FC7\u503C" + indexx)), false);
 		}
 		if (0 < indexx) {
+			if (0 < (entity instanceof Player _plr ? _plr.getFoodData().getSaturationLevel() : 0)) {
+				if (entity instanceof Player _player)
+					_player.getFoodData().setSaturation((float) ((entity instanceof Player _plr ? _plr.getFoodData().getSaturationLevel() : 0) - 1));
+			} else if (0 < (entity instanceof Player _plr ? _plr.getFoodData().getFoodLevel() : 0)) {
+				if (entity instanceof Player _player)
+					_player.getFoodData().setFoodLevel((entity instanceof Player _plr ? _plr.getFoodData().getFoodLevel() : 0) - 1);
+			} else if (entity.isAlive()) {
+				entity.invulnerableTime = 0;
+				if (entity instanceof LivingEntity _entity) {
+					DamageSource _dmgsource = new DamageSource(world.holderOrThrow(DamageTypes.GENERIC));
+					_entity.hurt(new DamageSource(_dmgsource.typeHolder(), _dmgsource.getEntity(), _dmgsource.getDirectEntity()) {
+						@Override
+						public Component getLocalizedDeathMessage(LivingEntity _msgEntity) {
+							String _translatekey = "death.attack.skip";
+							if (this.getEntity() == null && this.getDirectEntity() == null) {
+								return _msgEntity.getKillCredit() != null
+										? Component.translatable(_translatekey + ".player", _msgEntity.getDisplayName(), _msgEntity.getKillCredit().getDisplayName())
+										: Component.translatable(_translatekey, _msgEntity.getDisplayName());
+							} else {
+								Component _component = this.getEntity() == null ? this.getDirectEntity().getDisplayName() : this.getEntity().getDisplayName();
+								ItemStack _itemstack = ItemStack.EMPTY;
+								if (this.getEntity() instanceof LivingEntity _livingentity)
+									_itemstack = _livingentity.getMainHandItem();
+								return !_itemstack.isEmpty() && _itemstack.get(DataComponents.CUSTOM_NAME) != null
+										? Component.translatable(_translatekey + ".item", _msgEntity.getDisplayName(), _component, _itemstack.getDisplayName())
+										: Component.translatable(_translatekey, _msgEntity.getDisplayName(), _component);
+							}
+						}
+					}, (world.getLevelData().getGameRules().getInt(OneiricconceptModGameRules.OC_HEALTHMULTIPLIER)));
+				}
+			}
 			OneiricconceptMod.queueServerWork(1, () -> {
-				SkipdamageProcedure.execute(world, entity, itemstack, indexx - 1);
+				SkipdamageProcedure.execute(world, entity, itemstack, false, indexx - 1);
 			});
 		} else {
 			entity.invulnerableTime = 10;
--- src/main/java/net/mcreator/oneiricconcept/procedures/SkiplifeProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/SkiplifeProcedure.java
@@ -7,10 +7,14 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.component.DataComponents;
+
+import net.mcreator.oneiricconcept.init.OneiricconceptModGameRules;
 
 public class SkiplifeProcedure {
 	public static void execute(LevelAccessor world, Entity entity, ItemStack itemstack) {
@@ -20,19 +24,30 @@ public class SkiplifeProcedure {
 		double ktime = 0;
 		double index1 = 0;
 		if (entity instanceof Player _player)
-			_player.getCooldowns().addCooldown(itemstack.getItem(), 100);
+			_player.getCooldowns().addCooldown(itemstack.getItem(), 20);
+		if (entity instanceof LivingEntity _entity)
+			_entity.removeAllEffects();
 		for (int index0 = 0; index0 < 36; index0++) {
-			zpitem = (entity.getCapability(Capabilities.ItemHandler.ENTITY, null) instanceof IItemHandlerModifiable _modHandler2 ? _modHandler2.getStackInSlot((int) index0).copy() : ItemStack.EMPTY);
+			zpitem = (entity.getCapability(Capabilities.ItemHandler.ENTITY, null) instanceof IItemHandlerModifiable _modHandler3 ? _modHandler3.getStackInSlot((int) index0).copy() : ItemStack.EMPTY);
 			if (zpitem.getItem() == BuiltInRegistries.ITEM.get(ResourceLocation.parse("primogemcraft:xyzp")) && zpitem.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getBoolean("dai_kai_jiang")) {
-				ktime = itemstack.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("k1");
+				ktime = zpitem.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("k1");
+				if (world.getLevelData().getGameRules().getBoolean(OneiricconceptModGameRules.OCDEBUG)) {
+					if (!world.isClientSide() && world.getServer() != null)
+						world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("\u5728\u683C\uFF1A" + index0 + "\u6709\u4E13\u7968\u65F6\u95F4\u4E3A\uFF1A" + ktime)), false);
+				}
 				while (ktime < 24000) {
-					index1 = index1 + 1;
+					index1 = index1 + zpitem.getCount();
 					ktime = ktime + 1200;
 					{
 						final String _tagName = "k1";
 						final double _tagValue = ktime;
 						CustomData.update(DataComponents.CUSTOM_DATA, zpitem, tag -> tag.putDouble(_tagName, _tagValue));
 					}
+				}
+				{
+					final String _tagName = "k1";
+					final double _tagValue = 24000;
+					CustomData.update(DataComponents.CUSTOM_DATA, zpitem, tag -> tag.putDouble(_tagName, _tagValue));
 				}
 				if (entity.getCapability(Capabilities.ItemHandler.ENTITY, null) instanceof IItemHandlerModifiable _modHandler) {
 					ItemStack _setstack = zpitem.copy();
@@ -41,6 +56,6 @@ public class SkiplifeProcedure {
 				}
 			}
 		}
-		SkipdamageProcedure.execute(world, entity, itemstack, index1);
+		SkipdamageProcedure.execute(world, entity, itemstack, true, index1);
 	}
 }
