615890a5aa0edca9f0e07ccd619a04179a344a2c
Stella <1130668892@qq.com>
2025-11-03 23:18:39 +0800

    修改异世界木去皮流程

--- src/main/java/net/mcreator/oneiricconcept/procedures/StrippedProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/StrippedProcedure.java
@@ -1,46 +1,53 @@
 package net.mcreator.oneiricconcept.procedures;
 
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.tags.ItemTags;
-import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.BlockPos;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.CommandSource;
 
 import net.mcreator.oneiricconcept.init.OneiricconceptModBlocks;
 
 public class StrippedProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+	public static InteractionResult execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
-			return;
-		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("minecraft:axes")))
-				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("minecraft:axes")))) {
-			{
-				BlockPos _bp = BlockPos.containing(x, y, z);
-				BlockState _bs = OneiricconceptModBlocks.STRIPPED_THE_ANOTHER_WORLD_LOG.get().defaultBlockState();
-				BlockState _bso = world.getBlockState(_bp);
-				for (Property<?> _propertyOld : _bso.getProperties()) {
-					Property _propertyNew = _bs.getBlock().getStateDefinition().getProperty(_propertyOld.getName());
-					if (_propertyNew != null && _bs.getValue(_propertyNew) != null)
-						try {
-							_bs = _bs.setValue(_propertyNew, _bso.getValue(_propertyOld));
-						} catch (Exception e) {
-						}
+			return InteractionResult.PASS;
+		if (!world.isClientSide()) {
+			if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("minecraft:axes")))
+					|| (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("minecraft:axes")))) {
+				{
+					BlockPos _bp = BlockPos.containing(x, y, z);
+					BlockState _bs = OneiricconceptModBlocks.STRIPPED_THE_ANOTHER_WORLD_LOG.get().defaultBlockState();
+					BlockState _bso = world.getBlockState(_bp);
+					for (Property<?> _propertyOld : _bso.getProperties()) {
+						Property _propertyNew = _bs.getBlock().getStateDefinition().getProperty(_propertyOld.getName());
+						if (_propertyNew != null && _bs.getValue(_propertyNew) != null)
+							try {
+								_bs = _bs.setValue(_propertyNew, _bso.getValue(_propertyOld));
+							} catch (Exception e) {
+							}
+					}
+					world.setBlock(_bp, _bs, 3);
 				}
-				world.setBlock(_bp, _bs, 3);
+				if (world instanceof Level _level) {
+					if (!_level.isClientSide()) {
+						_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.axe.strip")), SoundSource.BLOCKS, 1, 1);
+					} else {
+						_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("item.axe.strip")), SoundSource.BLOCKS, 1, 1, false);
+					}
+				}
+			} else {
+				return InteractionResult.PASS;
 			}
-			if (world instanceof ServerLevel _level)
-				_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-						"playsound minecraft:item.axe.strip master @p ~ ~ ~ 16 0 0");
 		}
+		return InteractionResult.SUCCESS;
 	}
 }
