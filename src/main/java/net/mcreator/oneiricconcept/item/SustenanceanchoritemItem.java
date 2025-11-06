8740de6f43afa5d733884913aa0955a06bbc4c57
Stella <1130668892@qq.com>
2025-11-07 00:14:12 +0800

    缩短维生定锚展开时间

--- src/main/java/net/mcreator/oneiricconcept/item/SustenanceanchoritemItem.java
+++ src/main/java/net/mcreator/oneiricconcept/item/SustenanceanchoritemItem.java
@@ -29,7 +29,7 @@ public class SustenanceanchoritemItem extends Item {
 	@Override
 	public InteractionResult useOn(UseOnContext context) {
 		super.useOn(context);
-		SustenanceanchorPlaceProcedure.execute(context.getLevel(), context.getClickedPos().getX(), context.getClickedPos().getY(), context.getClickedPos().getZ(), context.getPlayer());
+		SustenanceanchorPlaceProcedure.execute(context.getLevel(), context.getClickedPos().getX(), context.getClickedPos().getY(), context.getClickedPos().getZ(), context.getPlayer(), context.getItemInHand());
 		return InteractionResult.SUCCESS;
 	}
 }
--- src/main/java/net/mcreator/oneiricconcept/procedures/SustenanceanchorPlaceProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/SustenanceanchorPlaceProcedure.java
@@ -9,12 +9,11 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
-import net.mcreator.oneiricconcept.init.OneiricconceptModItems;
 import net.mcreator.oneiricconcept.init.OneiricconceptModBlocks;
 import net.mcreator.oneiricconcept.OneiricconceptMod;
 
 public class SustenanceanchorPlaceProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
 		if (entity == null)
 			return;
 		Direction wsen = Direction.NORTH;
@@ -23,18 +22,18 @@ public class SustenanceanchorPlaceProcedure {
 		if (!world.isClientSide()) {
 			if (world.isEmptyBlock(BlockPos.containing(x, y + 1, z)) && world.isEmptyBlock(BlockPos.containing(x, y + 2, z)) && world.isEmptyBlock(BlockPos.containing(x, y + 3, z))) {
 				if (entity instanceof Player _player) {
-					ItemStack _stktoremove = new ItemStack(OneiricconceptModItems.SUSTENANCEANCHORITEM.get());
+					ItemStack _stktoremove = itemstack;
 					_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
 				}
 				WesnProcedure.execute(world, x, y, z, entity.getYRot());
-				for (int index0 = 0; index0 < 100; index0++) {
+				for (int index0 = 0; index0 < 10; index0++) {
 					nu = nu + 2;
 					OneiricconceptMod.queueServerWork((int) nu, () -> {
 						if (world instanceof ServerLevel _level)
 							_level.sendParticles(ParticleTypes.TOTEM_OF_UNDYING, (x + 0.5), (y + 3), (z + 0.5), 10, 0.2, 0.6, 0.2, 0.1);
 					});
 				}
-				OneiricconceptMod.queueServerWork(200, () -> {
+				OneiricconceptMod.queueServerWork(20, () -> {
 					world.setBlock(BlockPos.containing(x, y + 2, z), OneiricconceptModBlocks.SUSTENANCEANCHOROFF.get().defaultBlockState(), 3);
 				});
 			}
