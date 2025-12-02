b54b85f0ebd5094b79b087804b99a3611debcfa4
Stella <1130668892@qq.com>
2025-12-03 00:14:32 +0800

    冷却不抗关闭，移除

--- src/main/java/net/mcreator/oneiricconcept/procedures/ThunderSummonProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/ThunderSummonProcedure.java
@@ -2,10 +2,7 @@ package net.mcreator.oneiricconcept.procedures;
 
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.EntityType;
@@ -17,56 +14,25 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.oneiricconcept.init.OneiricconceptModGameRules;
-import net.mcreator.oneiricconcept.OneiricconceptMod;
 
 import java.util.Comparator;
 
 public class ThunderSummonProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		double damm = 0;
-		if (getBlockNBTLogic(world, BlockPos.containing(x, y, z), "enable")) {
-			if (!world.isClientSide()) {
-				BlockPos _bp = BlockPos.containing(x, y, z);
-				BlockEntity _blockEntity = world.getBlockEntity(_bp);
-				BlockState _bs = world.getBlockState(_bp);
-				if (_blockEntity != null)
-					_blockEntity.getPersistentData().putBoolean("enable", false);
-				if (world instanceof Level _level)
-					_level.sendBlockUpdated(_bp, _bs, _bs, 3);
-			}
-			if (world instanceof ServerLevel _level) {
-				LightningBolt entityToSpawn = EntityType.LIGHTNING_BOLT.create(_level);
-				entityToSpawn.moveTo(Vec3.atBottomCenterOf(BlockPos.containing(x, y + 1, z)));
-				entityToSpawn.setVisualOnly(true);
-				_level.addFreshEntity(entityToSpawn);
-			}
-			{
-				final Vec3 _center = new Vec3(x, y, z);
-				for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(5 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
-					if (!(entityiterator instanceof ItemEntity)) {
-						entityiterator.hurt(new DamageSource(world.holderOrThrow(DamageTypes.LIGHTNING_BOLT)), (float) (6 * (world.getLevelData().getGameRules().getInt(OneiricconceptModGameRules.OC_DAMAGEMULTIPLIER))
-								* (entityiterator.getType().is(EntityTypeTags.AQUATIC) ? 2 : 1) * (world.getLevelData().isThundering() ? 2 : 1)));
-					}
-				}
-			}
-			OneiricconceptMod.queueServerWork(10, () -> {
-				if (!world.isClientSide()) {
-					BlockPos _bp = BlockPos.containing(x, y, z);
-					BlockEntity _blockEntity = world.getBlockEntity(_bp);
-					BlockState _bs = world.getBlockState(_bp);
-					if (_blockEntity != null)
-						_blockEntity.getPersistentData().putBoolean("enable", true);
-					if (world instanceof Level _level)
-						_level.sendBlockUpdated(_bp, _bs, _bs, 3);
-				}
-			});
+		if (world instanceof ServerLevel _level) {
+			LightningBolt entityToSpawn = EntityType.LIGHTNING_BOLT.create(_level);
+			entityToSpawn.moveTo(Vec3.atBottomCenterOf(BlockPos.containing(x, y + 1, z)));
+			entityToSpawn.setVisualOnly(true);
+			_level.addFreshEntity(entityToSpawn);
 		}
-	}
-
-	private static boolean getBlockNBTLogic(LevelAccessor world, BlockPos pos, String tag) {
-		BlockEntity blockEntity = world.getBlockEntity(pos);
-		if (blockEntity != null)
-			return blockEntity.getPersistentData().getBoolean(tag);
-		return false;
+		{
+			final Vec3 _center = new Vec3(x, y, z);
+			for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(5 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
+				if (!(entityiterator instanceof ItemEntity)) {
+					entityiterator.hurt(new DamageSource(world.holderOrThrow(DamageTypes.LIGHTNING_BOLT)),
+							(float) (6 * (world.getLevelData().getGameRules().getInt(OneiricconceptModGameRules.OC_DAMAGEMULTIPLIER)) * (entityiterator.getType().is(EntityTypeTags.AQUATIC) ? 2 : 1) * (world.getLevelData().isThundering() ? 2 : 1)));
+				}
+			}
+		}
 	}
 }
