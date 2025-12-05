a12fa634b3bdd22ee3bbbe1c89dd87c761ca6adf
Pepper MR <zheshixbde@outlook.com>
2025-12-06 00:51:14 +0800

    草忘了

--- src/main/java/net/mcreator/oneiricconcept/procedures/EventPGCProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/EventPGCProcedure.java
@@ -33,7 +33,7 @@ public class EventPGCProcedure {
         EventRegistry.registerEvent(2002, ctx -> {OccurrencesTheMarshmallowProcedure.execute(ctx.getWorld(),ctx.getPlayer(),1);return true;});
         EventRegistry.registerEvent(2003, ctx -> { OccurrencesTheMarshmallowProcedure.execute(ctx.getWorld(),ctx.getPlayer(),2);return true;});
         EventRegistry.registerEvent(2004, ctx -> ctx.spawnEntitiesInRange(BARYON.get(), 3, 20, null, false, entity -> {ctx.applyEntityModifier(entity, livingEntity -> {ctx.invokeKillAll(livingEntity, 2, _true -> {ctx.createSimpleGroup(8, EventGroupProcedure.getRandomRegisteredEventId(ctx.getWorld()), 2000, "§a奖励");});});}));
-        EventRegistry.registerEvent(2005, ctx -> {WhiteDiamondFakeProcedure.execute(ctx.getWorld(),ctx.getPlayer().getX(),ctx.getPlayer().getY(),ctx.getPlayer().getZ());ctx.setGuiItem(new ItemStack(PrimogemcraftModItems.QWYZZM.get()), 1, 1, 4);return true;});
+        EventRegistry.registerEvent(2005, ctx -> {WhiteDiamondFakeProcedure.execute(ctx.getWorld(), ctx.getPlayer().getX(), ctx.getPlayer().getY(), ctx.getPlayer().getZ(), ctx.getPlayer());ctx.setGuiItem(new ItemStack(PrimogemcraftModItems.QWYZZM.get()), 1, 1, 4);return true;});
         //事件组
         EventRegistry.registerGroup(1000, ctx -> ctx.zu(2000, 2001, 7, "§e我不吃牛肉...吗？"));
         EventRegistry.registerGroup(1001, ctx -> ctx.zu(2002, 2003, 0, "§c飞向高天"));
--- src/main/java/net/mcreator/oneiricconcept/procedures/WhiteDiamondFakeProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/WhiteDiamondFakeProcedure.java
@@ -4,6 +4,7 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
@@ -17,14 +18,16 @@ import net.minecraft.commands.CommandSource;
 import java.util.Optional;
 
 public class WhiteDiamondFakeProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z) {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
 		if (world instanceof ServerLevel _level && _level.getServer() != null) {
 			Optional<CommandFunction<CommandSourceStack>> _fopt = _level.getServer().getFunctions().get(ResourceLocation.parse("oneiricconcept:big_red"));
 			if (_fopt.isPresent())
 				_level.getServer().getFunctions().execute(_fopt.get(), new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null));
 		}
 		if (!world.isClientSide() && world.getServer() != null)
-			world.getServer().getPlayerList().broadcastSystemMessage(Component.literal("\u5B8C\u6210\u4E86\u6311\u6218\u00A75[\u4E00\u68F5\u6C38\u342C\u4E13]"), false);
+			world.getServer().getPlayerList().broadcastSystemMessage(Component.literal((entity.getDisplayName().getString() + "\u5B8C\u6210\u4E86\u6311\u6218\u00A75[\u4E00\u68F5\u6C38\u342C\u4E13]")), false);
 		if (world instanceof Level _level) {
 			if (!_level.isClientSide()) {
 				_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("ui.toast.challenge_complete")), SoundSource.PLAYERS, 1, 1);
