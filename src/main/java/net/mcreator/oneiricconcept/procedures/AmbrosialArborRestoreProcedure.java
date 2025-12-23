7b3e3b689244577e88d1840f16b6b406a4322492
Stella <1130668892@qq.com>
2025-12-23 18:10:03 +0800

    改动建木方块被破坏时的逻辑
    取消事件并掉落物品->放置新的对应方块

--- src/main/java/net/mcreator/oneiricconcept/procedures/AmbrosialArborRestoreProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/AmbrosialArborRestoreProcedure.java
@@ -3,7 +3,6 @@ package net.mcreator.oneiricconcept.procedures;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.bus.api.ICancellableEvent;
 import net.neoforged.bus.api.Event;
 
 import net.minecraft.world.level.block.state.BlockState;
@@ -12,14 +11,13 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.client.Minecraft;
 
@@ -47,14 +45,7 @@ public class AmbrosialArborRestoreProcedure {
 		if (blockstate.is(BlockTags.create(ResourceLocation.parse("c:ambrosialarbor"))) && !(getEntityGameType(ent) == GameType.CREATIVE)
 				&& !((ent instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("oneiricconcept:ignisaureliae"))))) {
 			if (Math.random() < 0.75) {
-				if (event instanceof ICancellableEvent _cancellable) {
-					_cancellable.setCanceled(true);
-				}
-				if (world instanceof ServerLevel _level) {
-					ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, (new ItemStack(blockstate.getBlock())));
-					entityToSpawn.setPickUpDelay(10);
-					_level.addFreshEntity(entityToSpawn);
-				}
+				world.setBlock(BlockPos.containing(x, y, z), blockstate, 3);
 			}
 			SummonzombieProcedure.execute(world, ent.getX(), ent.getY() + 3, ent.getZ());
 		}
