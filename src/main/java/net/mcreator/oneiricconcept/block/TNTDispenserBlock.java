f46e76b0b2b8127cc030261d7cf8ab2d86a39b02
Stella <1130668892@qq.com>
2025-12-25 23:01:30 +0800

    tnt发射器可以旋转

--- src/main/java/net/mcreator/oneiricconcept/block/TNTDispenserBlock.java
+++ src/main/java/net/mcreator/oneiricconcept/block/TNTDispenserBlock.java
@@ -1,23 +1,51 @@
 package net.mcreator.oneiricconcept.block;
 
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.oneiricconcept.procedures.TNTlaunchProcedure;
 
 public class TNTDispenserBlock extends Block {
+	public static final DirectionProperty FACING = DirectionalBlock.FACING;
+
 	public TNTDispenserBlock() {
 		super(BlockBehaviour.Properties.of().strength(1f, 999f));
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
 	}
 
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
 		return 15;
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(FACING);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return super.getStateForPlacement(context).setValue(FACING, context.getNearestLookingDirection().getOpposite());
+	}
+
+	public BlockState rotate(BlockState state, Rotation rot) {
+		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+	}
+
+	public BlockState mirror(BlockState state, Mirror mirrorIn) {
+		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
 	}
 
 	@Override
@@ -29,7 +57,7 @@ public class TNTDispenserBlock extends Block {
 	public void neighborChanged(BlockState blockstate, Level world, BlockPos pos, Block neighborBlock, BlockPos fromPos, boolean moving) {
 		super.neighborChanged(blockstate, world, pos, neighborBlock, fromPos, moving);
 		if (world.getBestNeighborSignal(pos) > 0) {
-			TNTlaunchProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
+			TNTlaunchProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ(), blockstate);
 		}
 	}
 }
--- src/main/java/net/mcreator/oneiricconcept/procedures/TNTlaunchProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/TNTlaunchProcedure.java
@@ -1,20 +1,39 @@
 package net.mcreator.oneiricconcept.procedures;
 
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 public class TNTlaunchProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z) {
-		if (!world.getBlockState(BlockPos.containing(x, y - 1, z)).canOcclude()) {
+	public static void execute(LevelAccessor world, double x, double y, double z, BlockState blockstate) {
+		double xx = 0;
+		double yy = 0;
+		double zz = 0;
+		xx = (getDirectionFromBlockState(blockstate)).getStepX() + x;
+		yy = (getDirectionFromBlockState(blockstate)).getStepY() + y;
+		zz = (getDirectionFromBlockState(blockstate)).getStepZ() + z;
+		if (!world.getBlockState(BlockPos.containing(xx, yy, zz)).canOcclude()) {
 			if (world instanceof ServerLevel _level) {
-				Entity entityToSpawn = EntityType.TNT.spawn(_level, BlockPos.containing(x, y - 1, z), MobSpawnType.MOB_SUMMONED);
+				Entity entityToSpawn = EntityType.TNT.spawn(_level, BlockPos.containing(xx, yy, zz), MobSpawnType.MOB_SUMMONED);
 				if (entityToSpawn != null) {
 				}
 			}
 		}
+	}
+
+	private static Direction getDirectionFromBlockState(BlockState blockState) {
+		Property<?> prop = blockState.getBlock().getStateDefinition().getProperty("facing");
+		if (prop instanceof DirectionProperty dp)
+			return blockState.getValue(dp);
+		prop = blockState.getBlock().getStateDefinition().getProperty("axis");
+		return prop instanceof EnumProperty ep && ep.getPossibleValues().toArray()[0] instanceof Direction.Axis ? Direction.fromAxisAndDirection((Direction.Axis) blockState.getValue(ep), Direction.AxisDirection.POSITIVE) : Direction.NORTH;
 	}
 }
