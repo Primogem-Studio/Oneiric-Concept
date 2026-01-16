ce1bdc2c3b5854ef3c4469ff5db050557fd69cb6
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2026-01-16 23:03:02 +0800

    乘区影响范围

--- src/main/java/net/mcreator/oneiricconcept/procedures/ThunderSummonProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/ThunderSummonProcedure.java
@@ -12,7 +12,6 @@ import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.network.chat.Component;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.oneiricconcept.init.OneiricconceptModGameRules;
@@ -21,23 +20,20 @@ import java.util.Comparator;
 
 public class ThunderSummonProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
+		double lvl = 0;
 		if (world instanceof ServerLevel _level) {
 			LightningBolt entityToSpawn = EntityType.LIGHTNING_BOLT.create(_level);
 			entityToSpawn.moveTo(Vec3.atBottomCenterOf(BlockPos.containing(x, y, z)));
 			entityToSpawn.setVisualOnly(true);
 			_level.addFreshEntity(entityToSpawn);
 		}
+		lvl = (world.getLevelData().isThundering() ? 2 : 1) * ((world.getFluidState(BlockPos.containing(x, y, z)).createLegacyBlock()).getBlock() instanceof LiquidBlock ? 2 : 1);
 		{
 			final Vec3 _center = new Vec3(x, y, z);
-			for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(5 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
+			for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate((4 + lvl) / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
 				if (!(entityiterator instanceof ItemEntity)) {
-					entityiterator.hurt(new DamageSource(world.holderOrThrow(DamageTypes.LIGHTNING_BOLT)),
-							(float) (6 * (world.getLevelData().getGameRules().getInt(OneiricconceptModGameRules.OC_DAMAGEMULTIPLIER)) * (world.getLevelData().isThundering() ? 2 : 1) * (entityiterator.getType().is(EntityTypeTags.AQUATIC) ? 2 : 1)
-									* ((world.getFluidState(BlockPos.containing(x, y, z)).createLegacyBlock()).getBlock() instanceof LiquidBlock ? 2 : 1) * (entityiterator.isInWaterRainOrBubble() ? 3 : 1)));
-				}
-				if (world.getLevelData().getGameRules().getBoolean(OneiricconceptModGameRules.OCDEBUG)) {
-					if (!world.isClientSide() && world.getServer() != null)
-						world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("" + ((world.getFluidState(BlockPos.containing(x, y, z)).createLegacyBlock()).getBlock() instanceof LiquidBlock))), false);
+					entityiterator.hurt(new DamageSource(world.holderOrThrow(DamageTypes.LIGHTNING_BOLT)), (float) (6 * (world.getLevelData().getGameRules().getInt(OneiricconceptModGameRules.OC_DAMAGEMULTIPLIER)) * lvl
+							* (entityiterator.getType().is(EntityTypeTags.AQUATIC) ? 2 : 1) * (entityiterator.isInWaterRainOrBubble() ? 3 : 1)));
 				}
 			}
 		}
