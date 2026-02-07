e08d50c584fbbc0fec2f8d0e73f610fea0f3f160
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2026-02-07 20:39:25 +0800

    发射器更新

--- src/main/java/net/mcreator/oneiricconcept/procedures/DendrolaunchplaceProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/DendrolaunchplaceProcedure.java
@@ -2,6 +2,7 @@ package net.mcreator.oneiricconcept.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.GameType;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
@@ -10,14 +11,21 @@ import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.BlockPos;
+import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.client.Minecraft;
 
 import net.mcreator.oneiricconcept.init.OneiricconceptModItems;
 import net.mcreator.oneiricconcept.init.OneiricconceptModEntities;
+import net.mcreator.oneiricconcept.entity.ExplosiveHydroEntity;
 import net.mcreator.oneiricconcept.entity.ExplosiveEntity;
+import net.mcreator.oneiricconcept.entity.ExplosiveElectroEntity;
+import net.mcreator.oneiricconcept.entity.ExplosiveDendroEntity;
+import net.mcreator.oneiricconcept.entity.ExplosiveCryoEntity;
 import net.mcreator.oneiricconcept.entity.DendroCoreEntity;
 
 public class DendrolaunchplaceProcedure {
@@ -50,18 +58,82 @@ public class DendrolaunchplaceProcedure {
 					projectileLevel.addFreshEntity(_entityToSpawn);
 				}
 			}
+		} else if (OneiricconceptModItems.CRYO_BOOMYAM.get() == item.getItem()) {
+			{
+				Entity _shootFrom = entity;
+				Level projectileLevel = _shootFrom.level();
+				if (!projectileLevel.isClientSide()) {
+					Projectile _entityToSpawn = initArrowProjectile(new ExplosiveCryoEntity(OneiricconceptModEntities.EXPLOSIVE_CRYO.get(), 0, 0, 0, projectileLevel, createArrowWeaponItemStack(projectileLevel, 1, (byte) 0)), entity, 5, true, false,
+							false, AbstractArrow.Pickup.DISALLOWED);
+					_entityToSpawn.setPos(_shootFrom.getX(), _shootFrom.getEyeY() - 0.1, _shootFrom.getZ());
+					_entityToSpawn.shoot(_shootFrom.getLookAngle().x, _shootFrom.getLookAngle().y, _shootFrom.getLookAngle().z, 2, 0);
+					projectileLevel.addFreshEntity(_entityToSpawn);
+				}
+			}
+		} else if (OneiricconceptModItems.DENDRO_BOOMYAM.get() == item.getItem()) {
+			{
+				Entity _shootFrom = entity;
+				Level projectileLevel = _shootFrom.level();
+				if (!projectileLevel.isClientSide()) {
+					Projectile _entityToSpawn = initArrowProjectile(new ExplosiveDendroEntity(OneiricconceptModEntities.EXPLOSIVE_DENDRO.get(), 0, 0, 0, projectileLevel, createArrowWeaponItemStack(projectileLevel, 1, (byte) 0)), entity, 5, true,
+							false, false, AbstractArrow.Pickup.DISALLOWED);
+					_entityToSpawn.setPos(_shootFrom.getX(), _shootFrom.getEyeY() - 0.1, _shootFrom.getZ());
+					_entityToSpawn.shoot(_shootFrom.getLookAngle().x, _shootFrom.getLookAngle().y, _shootFrom.getLookAngle().z, 2, 0);
+					projectileLevel.addFreshEntity(_entityToSpawn);
+				}
+			}
+		} else if (OneiricconceptModItems.ELECTRO_BOOMYAM.get() == item.getItem()) {
+			{
+				Entity _shootFrom = entity;
+				Level projectileLevel = _shootFrom.level();
+				if (!projectileLevel.isClientSide()) {
+					Projectile _entityToSpawn = initArrowProjectile(new ExplosiveElectroEntity(OneiricconceptModEntities.EXPLOSIVE_ELECTRO.get(), 0, 0, 0, projectileLevel, createArrowWeaponItemStack(projectileLevel, 1, (byte) 0)), entity, 5, true,
+							false, false, AbstractArrow.Pickup.DISALLOWED);
+					_entityToSpawn.setPos(_shootFrom.getX(), _shootFrom.getEyeY() - 0.1, _shootFrom.getZ());
+					_entityToSpawn.shoot(_shootFrom.getLookAngle().x, _shootFrom.getLookAngle().y, _shootFrom.getLookAngle().z, 2, 0);
+					projectileLevel.addFreshEntity(_entityToSpawn);
+				}
+			}
+		} else if (OneiricconceptModItems.HYDRO_BOOMYAM.get() == item.getItem()) {
+			{
+				Entity _shootFrom = entity;
+				Level projectileLevel = _shootFrom.level();
+				if (!projectileLevel.isClientSide()) {
+					Projectile _entityToSpawn = initArrowProjectile(new ExplosiveHydroEntity(OneiricconceptModEntities.EXPLOSIVE_HYDRO.get(), 0, 0, 0, projectileLevel, createArrowWeaponItemStack(projectileLevel, 1, (byte) 0)), entity, 5, true, false,
+							false, AbstractArrow.Pickup.DISALLOWED);
+					_entityToSpawn.setPos(_shootFrom.getX(), _shootFrom.getEyeY() - 0.1, _shootFrom.getZ());
+					_entityToSpawn.shoot(_shootFrom.getLookAngle().x, _shootFrom.getLookAngle().y, _shootFrom.getLookAngle().z, 2, 0);
+					projectileLevel.addFreshEntity(_entityToSpawn);
+				}
+			}
 		} else {
-			if (world instanceof Level _level) {
-				if (!_level.isClientSide()) {
-					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.dispenser.fail")), SoundSource.PLAYERS, 1, 1);
-				} else {
-					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.dispenser.fail")), SoundSource.PLAYERS, 1, 1, false);
+			if (getEntityGameType(entity) == GameType.CREATIVE) {
+				{
+					Entity _shootFrom = entity;
+					Level projectileLevel = _shootFrom.level();
+					if (!projectileLevel.isClientSide()) {
+						Projectile _entityToSpawn = initArrowProjectile(new DendroCoreEntity(OneiricconceptModEntities.DENDRO_CORE.get(), 0, 0, 0, projectileLevel, createArrowWeaponItemStack(projectileLevel, 1, (byte) 3)), entity, 2, true, false,
+								false, AbstractArrow.Pickup.DISALLOWED);
+						_entityToSpawn.setPos(_shootFrom.getX(), _shootFrom.getEyeY() - 0.1, _shootFrom.getZ());
+						_entityToSpawn.shoot(_shootFrom.getLookAngle().x, _shootFrom.getLookAngle().y, _shootFrom.getLookAngle().z, 2, 0);
+						projectileLevel.addFreshEntity(_entityToSpawn);
+					}
+				}
+			} else {
+				if (world instanceof Level _level) {
+					if (!_level.isClientSide()) {
+						_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.dispenser.fail")), SoundSource.PLAYERS, 1, 1);
+					} else {
+						_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.dispenser.fail")), SoundSource.PLAYERS, 1, 1, false);
+					}
 				}
 			}
 		}
-		if (entity instanceof Player _player) {
-			ItemStack _stktoremove = item;
-			_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+		if (!(getEntityGameType(entity) == GameType.CREATIVE)) {
+			if (entity instanceof Player _player) {
+				ItemStack _stktoremove = item;
+				_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+			}
 		}
 	}
 
@@ -85,5 +157,16 @@ public class DendrolaunchplaceProcedure {
 		if (piercing > 0)
 			weapon.enchant(level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.PIERCING), piercing);
 		return weapon;
+	}
+
+	private static GameType getEntityGameType(Entity entity) {
+		if (entity instanceof ServerPlayer serverPlayer) {
+			return serverPlayer.gameMode.getGameModeForPlayer();
+		} else if (entity instanceof Player player && player.level().isClientSide()) {
+			PlayerInfo playerInfo = Minecraft.getInstance().getConnection().getPlayerInfo(player.getGameProfile().getId());
+			if (playerInfo != null)
+				return playerInfo.getGameMode();
+		}
+		return null;
 	}
 }
