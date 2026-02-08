482f842b0bceaa68da39bff6447f06c865be3824
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2026-02-08 18:25:27 +0800

    修复悬浮文字清除流程

--- src/main/java/net/mcreator/oneiricconcept/procedures/XiaolanternUnPlaceProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/XiaolanternUnPlaceProcedure.java
@@ -12,7 +12,7 @@ public class XiaolanternUnPlaceProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		{
 			final Vec3 _center = new Vec3(x, (y + 1), z);
-			for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(1 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
+			for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(2 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
 				if (entityiterator instanceof Display.TextDisplay) {
 					if (!entityiterator.level().isClientSide())
 						entityiterator.discard();
