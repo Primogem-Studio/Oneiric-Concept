b3300b7ea1ebe8b51a26d943123bdde6c8cf2dc3
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2026-04-18 18:46:50 +0800

    在非困难模式下的玩家受到爆爆瓜等范围伤害减半

--- src/main/java/net/mcreator/oneiricconcept/procedures/RangeDamageProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/RangeDamageProcedure.java
@@ -3,8 +3,10 @@ package net.mcreator.oneiricconcept.procedures;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.Difficulty;
 
 import net.mcreator.oneiricconcept.init.OneiricconceptModGameRules;
 
@@ -17,7 +19,8 @@ public class RangeDamageProcedure {
 		{
 			final Vec3 _center = new Vec3(x, y, z);
 			for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(range / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
-				entityiterator.hurt(damagesource, (float) (damage * (world.getLevelData().getGameRules().getInt(OneiricconceptModGameRules.OC_HEALTHMULTIPLIER))));
+				entityiterator.hurt(damagesource,
+						(float) ((world.getDifficulty() == Difficulty.HARD && !(entityiterator instanceof Player) ? damage : damage * 0.5) * (world.getLevelData().getGameRules().getInt(OneiricconceptModGameRules.OC_HEALTHMULTIPLIER))));
 			}
 		}
 	}
