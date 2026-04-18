d094a53b9f3f757784ad534d0a05d86c77da7e01
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2026-04-18 20:20:14 +0800

    爆爆瓜对玩家的宠物也伤害减半

--- src/main/java/net/mcreator/oneiricconcept/procedures/RangeDamageProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/RangeDamageProcedure.java
@@ -4,6 +4,7 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.Difficulty;
@@ -20,7 +21,8 @@ public class RangeDamageProcedure {
 			final Vec3 _center = new Vec3(x, y, z);
 			for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(range / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
 				entityiterator.hurt(damagesource,
-						(float) ((world.getDifficulty() == Difficulty.HARD && !(entityiterator instanceof Player) ? damage : damage * 0.5) * (world.getLevelData().getGameRules().getInt(OneiricconceptModGameRules.OC_HEALTHMULTIPLIER))));
+						(float) ((world.getDifficulty() == Difficulty.HARD && !(entityiterator instanceof Player || (entityiterator instanceof TamableAnimal _tamEnt ? (Entity) _tamEnt.getOwner() : null) instanceof Player) ? damage : damage * 0.5)
+								* (world.getLevelData().getGameRules().getInt(OneiricconceptModGameRules.OC_HEALTHMULTIPLIER))));
 			}
 		}
 	}
