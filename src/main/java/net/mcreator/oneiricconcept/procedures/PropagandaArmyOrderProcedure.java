99c1566aa57e47a97b952a095519b6d624274505
Lumine139637659 <123864592+Lumine139637659@users.noreply.github.com>
2026-05-02 13:51:40 +0800

    重写了2006随机文本逻辑和添加本地化适配

--- src/main/java/net/mcreator/oneiricconcept/procedures/PropagandaArmyOrderProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/PropagandaArmyOrderProcedure.java
@@ -16,6 +16,7 @@ import net.minecraft.core.component.DataComponents;
 
 import net.mcreator.oneiricconcept.network.OneiricconceptModVariables;
 import net.mcreator.oneiricconcept.init.OneiricconceptModItems;
+import net.mcreator.oneiricconcept.init.OneiricconceptModBlocks;
 import net.mcreator.oneiricconcept.OneiricconceptMod;
 
 public class PropagandaArmyOrderProcedure {
@@ -25,8 +26,7 @@ public class PropagandaArmyOrderProcedure {
 		String txt = "";
 		double Nu = 0;
 		double NuE = 0;
-		double TXTNU = 0;
-		if (hasEntityInInventory(entity, new ItemStack(OneiricconceptModItems.PROPAGANDA_ARMY.get()))) {
+		if (hasEntityInInventory(entity, new ItemStack(OneiricconceptModItems.PROPAGANDA_ARMY.get())) || hasEntityInInventory(entity, new ItemStack(OneiricconceptModBlocks.PROPAGANDA_ARMY_BLOCK.get()))) {
 			for (int index0 = 0; index0 < (int) (exp / 20); index0++) {
 				if (entity instanceof Player _player)
 					_player.giveExperiencePoints((int) exp);
@@ -41,14 +41,7 @@ public class PropagandaArmyOrderProcedure {
 				Nu = nu;
 				if (entity instanceof Player _player && !_player.level().isClientSide())
 					_player.displayClientMessage(Component.literal((sec + "s")), true);
-				TXTNU = Mth.nextInt(RandomSource.create(), 0, 4);
-				if (TXTNU < 1) {
-					txt = TXTNU < 0 ? "\u4E00\u751F\u53EA\u7231\u706B\u82B1\u82B1\uFF0C\u53D8\u5FC3\u4E00\u5B9A\u906D\u96F7\u5288\uFF01" : "\u809A\u76AE\u9971\u9971\u5FC3\u60C5\u597D\uFF0C\u770B\u706B\u82B1\u82B1\u770B\u5230\u8001\uFF01";
-				} else if (TXTNU < 3) {
-					txt = TXTNU < 2 ? "\u706B\u82B1\u82B1\uFF01\u706B\u82B1\u82B1\uFF01" : "\u72C2\u6B22\u76F4\u5230\u4E16\u754C\u5C3D\u5934\uFF01";
-				} else {
-					txt = "\u706B\u82B1\u82B1\u597D\u597D\u597D";
-				}
+				txt = Component.translatable(("translation.oneiricconcept.propaganda" + Mth.nextInt(RandomSource.create(), 1, 5))).getString();
 				if (is1) {
 					{
 						OneiricconceptModVariables.PlayerVariables _vars = entity.getData(OneiricconceptModVariables.PLAYER_VARIABLES);
