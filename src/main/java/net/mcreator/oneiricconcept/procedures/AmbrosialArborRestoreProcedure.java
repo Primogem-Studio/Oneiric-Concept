a5aae398e1dc07b6e621d56a4561a23f4e544898
steela <1130668892@qq.com>
2026-05-08 14:32:42 +0800

    优化建木破坏流程
    去除多余局部变量，试图增加寻猎附魔判断，但MCR不支持

--- src/main/java/net/mcreator/oneiricconcept/procedures/AmbrosialArborRestoreProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/AmbrosialArborRestoreProcedure.java
@@ -6,7 +6,6 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.item.ItemStack;
@@ -37,17 +36,13 @@ public class AmbrosialArborRestoreProcedure {
 	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, BlockState blockstate, Entity entity) {
 		if (entity == null)
 			return;
-		double restore = 0;
-		BlockState block = Blocks.AIR.defaultBlockState();
-		Entity ent = null;
-		String summons = "";
-		ent = entity;
-		if (blockstate.is(BlockTags.create(ResourceLocation.parse("c:ambrosialarbor"))) && !(getEntityGameType(ent) == GameType.CREATIVE)
-				&& !((ent instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(ResourceLocation.parse("oneiricconcept:ignisaureliae"))))) {
+		ItemStack axe = ItemStack.EMPTY;
+		axe = (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY);
+		if (!(getEntityGameType(entity) == GameType.CREATIVE) && !axe.is(ItemTags.create(ResourceLocation.parse("oneiricconcept:ignisaureliae"))) && blockstate.is(BlockTags.create(ResourceLocation.parse("c:ambrosialarbor")))) {
 			if (Math.random() < 0.75) {
 				world.setBlock(BlockPos.containing(x, y, z), blockstate, 3);
 			}
-			SummonzombieProcedure.execute(world, ent.getX(), ent.getY() + 3, ent.getZ());
+			SummonzombieProcedure.execute(world, entity.getX(), entity.getY() + 3, entity.getZ());
 		}
 	}
 
