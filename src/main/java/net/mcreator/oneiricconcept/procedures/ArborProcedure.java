4eeb5485a7c9b50108dad2fcb7a4c955273d533c
steela <1130668892@qq.com>
2026-06-22 17:24:14 +0800

    修改建木柄材催熟流程

--- src/main/java/net/mcreator/oneiricconcept/procedures/ArborProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/ArborProcedure.java
@@ -1,7 +1,8 @@
 package net.mcreator.oneiricconcept.procedures;
 
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.Items;
@@ -15,19 +16,27 @@ import net.minecraft.core.BlockPos;
 
 public class ArborProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		BlockState blocks = Blocks.AIR.defaultBlockState();
-		blocks = (world.getBlockState(BlockPos.containing(x, y, z)));
 		if (world instanceof ServerLevel _level)
 			_level.sendParticles(ParticleTypes.HAPPY_VILLAGER, (x + 0.5), (y + 0.5), (z + 0.5), 10, 0.3, 0.3, 0.3, 1);
+		{
+			int _value = (int) ((world.getBlockState(BlockPos.containing(x, y, z))).is(BlockTags.create(ResourceLocation.parse("minecraft:bee_growables"))) ? 7 : 3);
+			BlockPos _pos = BlockPos.containing(x, y, z);
+			BlockState _bs = world.getBlockState(_pos);
+			if (_bs.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
+				world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
+		}
+		{
+			BlockPos _pos = BlockPos.containing(x, y, z);
+			BlockState _bs = world.getBlockState(_pos);
+			if (_bs.getBlock().getStateDefinition().getProperty("berries") instanceof BooleanProperty _booleanProp)
+				world.setBlock(_pos, _bs.setValue(_booleanProp, true), 3);
+		}
 		if (world instanceof Level _level) {
 			BlockPos _bp = BlockPos.containing(x, y, z);
 			if (BoneMealItem.growCrop(new ItemStack(Items.BONE_MEAL), _level, _bp) || BoneMealItem.growWaterPlant(new ItemStack(Items.BONE_MEAL), _level, _bp, null)) {
 				if (!_level.isClientSide())
 					_level.levelEvent(2005, _bp, 0);
 			}
-		}
-		if ((blocks.is(BlockTags.create(ResourceLocation.parse("c:sugar_canes"))) || blocks.is(BlockTags.create(ResourceLocation.parse("c:cactus")))) && RandomProcedure.execute(world, 0.3)) {
-			world.setBlock(BlockPos.containing(x, y + 1, z), blocks, 3);
 		}
 	}
 }
