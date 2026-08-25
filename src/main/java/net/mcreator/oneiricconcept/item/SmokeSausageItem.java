3a2c534fcd16f8bd6cee720ed704ec8f66775742
steela <1130668892@qq.com>
2026-08-25 14:46:41 +0800

    修改生命回复效果，修改异界月金英文名大小写

--- src/main/java/net/mcreator/oneiricconcept/item/SmokeSausageItem.java
+++ src/main/java/net/mcreator/oneiricconcept/item/SmokeSausageItem.java
@@ -1,16 +1,31 @@
 package net.mcreator.oneiricconcept.item;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlotGroup;
+
+import net.mcreator.oneiricconcept.procedures.H10Procedure;
 
 public class SmokeSausageItem extends Item {
 	public SmokeSausageItem() {
 		super(new Item.Properties().food((new FoodProperties.Builder()).nutrition(12).saturationModifier(0.7f).build())
 				.attributes(ItemAttributeModifiers.builder().add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 4, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
 						.add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, -2.4, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).build()));
+	}
+
+	@Override
+	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+		ItemStack retval = super.finishUsingItem(itemstack, world, entity);
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		H10Procedure.execute(entity);
+		return retval;
 	}
 }
