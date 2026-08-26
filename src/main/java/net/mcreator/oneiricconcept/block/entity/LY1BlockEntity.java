991f0423ba778a0a7e6c153049973f208e3f7cd3
steela <1130668892@qq.com>
2026-08-26 20:24:11 +0800

    应该不太影响的MCR自动修改

--- src/main/java/net/mcreator/oneiricconcept/block/entity/LY1BlockEntity.java
+++ src/main/java/net/mcreator/oneiricconcept/block/entity/LY1BlockEntity.java
@@ -5,7 +5,6 @@ import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
@@ -28,24 +27,6 @@ import io.netty.buffer.Unpooled;
 
 public class LY1BlockEntity extends RandomizableContainerBlockEntity implements WorldlyContainer {
 	private NonNullList<ItemStack> stacks = NonNullList.withSize(9, ItemStack.EMPTY);
-	public final AnimationState animationState0 = new AnimationState();
-	public final AnimationState animationState1 = new AnimationState();
-	public final AnimationState animationState2 = new AnimationState();
-	public final AnimationState animationState3 = new AnimationState();
-	public final AnimationState animationState4 = new AnimationState();
-	public final AnimationState animationState5 = new AnimationState();
-	public final AnimationState animationState6 = new AnimationState();
-	public final AnimationState animationState7 = new AnimationState();
-	public final AnimationState animationState8 = new AnimationState();
-	public final AnimationState animationState9 = new AnimationState();
-	public final AnimationState animationState10 = new AnimationState();
-	public final AnimationState animationState11 = new AnimationState();
-	public final AnimationState animationState12 = new AnimationState();
-	public final AnimationState animationState13 = new AnimationState();
-	public final AnimationState animationState14 = new AnimationState();
-	public final AnimationState animationState15 = new AnimationState();
-	public final AnimationState animationState16 = new AnimationState();
-	public final AnimationState animationState17 = new AnimationState();
 
 	public LY1BlockEntity(BlockPos position, BlockState state) {
 		super(OneiricconceptModBlockEntities.LY_1.get(), position, state);
