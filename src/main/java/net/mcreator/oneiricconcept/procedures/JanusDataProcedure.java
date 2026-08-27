2f68a8146fdd10a65dd1c90744f03f3c3772b267
steela <1130668892@qq.com>
2026-08-27 14:19:17 +0800

    修正旋转

--- src/main/java/net/mcreator/oneiricconcept/procedures/JanusDataProcedure.java
+++ src/main/java/net/mcreator/oneiricconcept/procedures/JanusDataProcedure.java
@@ -49,64 +49,64 @@ public class JanusDataProcedure {
 				_setstack.setCount(1);
 				_itemHandlerModifiable.setStackInSlot(1, _setstack);
 			}
-		}
-		if (Math.abs(zz - z) <= Math.abs(xx - x)) {
-			if (0 < xx - x) {
-				{
-					Direction _dir = Direction.DOWN;
-					BlockPos _pos = BlockPos.containing(x, y, z);
-					BlockState _bs = world.getBlockState(_pos);
-					Property<?> _property = _bs.getBlock().getStateDefinition().getProperty("facing");
-					if (_property instanceof DirectionProperty _dp && _dp.getPossibleValues().contains(_dir)) {
-						world.setBlock(_pos, _bs.setValue(_dp, _dir), 3);
-					} else {
-						_property = _bs.getBlock().getStateDefinition().getProperty("axis");
-						if (_property instanceof EnumProperty _ap && _ap.getPossibleValues().contains(_dir.getAxis()))
-							world.setBlock(_pos, _bs.setValue(_ap, _dir.getAxis()), 3);
+			if (Math.abs(zz - z) <= Math.abs(xx - x)) {
+				if (0 < xx - x) {
+					{
+						Direction _dir = Direction.WEST;
+						BlockPos _pos = BlockPos.containing(x, y, z);
+						BlockState _bs = world.getBlockState(_pos);
+						Property<?> _property = _bs.getBlock().getStateDefinition().getProperty("facing");
+						if (_property instanceof DirectionProperty _dp && _dp.getPossibleValues().contains(_dir)) {
+							world.setBlock(_pos, _bs.setValue(_dp, _dir), 3);
+						} else {
+							_property = _bs.getBlock().getStateDefinition().getProperty("axis");
+							if (_property instanceof EnumProperty _ap && _ap.getPossibleValues().contains(_dir.getAxis()))
+								world.setBlock(_pos, _bs.setValue(_ap, _dir.getAxis()), 3);
+						}
+					}
+				} else {
+					{
+						Direction _dir = Direction.EAST;
+						BlockPos _pos = BlockPos.containing(x, y, z);
+						BlockState _bs = world.getBlockState(_pos);
+						Property<?> _property = _bs.getBlock().getStateDefinition().getProperty("facing");
+						if (_property instanceof DirectionProperty _dp && _dp.getPossibleValues().contains(_dir)) {
+							world.setBlock(_pos, _bs.setValue(_dp, _dir), 3);
+						} else {
+							_property = _bs.getBlock().getStateDefinition().getProperty("axis");
+							if (_property instanceof EnumProperty _ap && _ap.getPossibleValues().contains(_dir.getAxis()))
+								world.setBlock(_pos, _bs.setValue(_ap, _dir.getAxis()), 3);
+						}
 					}
 				}
 			} else {
-				{
-					Direction _dir = Direction.DOWN;
-					BlockPos _pos = BlockPos.containing(x, y, z);
-					BlockState _bs = world.getBlockState(_pos);
-					Property<?> _property = _bs.getBlock().getStateDefinition().getProperty("facing");
-					if (_property instanceof DirectionProperty _dp && _dp.getPossibleValues().contains(_dir)) {
-						world.setBlock(_pos, _bs.setValue(_dp, _dir), 3);
-					} else {
-						_property = _bs.getBlock().getStateDefinition().getProperty("axis");
-						if (_property instanceof EnumProperty _ap && _ap.getPossibleValues().contains(_dir.getAxis()))
-							world.setBlock(_pos, _bs.setValue(_ap, _dir.getAxis()), 3);
+				if (0 < zz - z) {
+					{
+						Direction _dir = Direction.NORTH;
+						BlockPos _pos = BlockPos.containing(x, y, z);
+						BlockState _bs = world.getBlockState(_pos);
+						Property<?> _property = _bs.getBlock().getStateDefinition().getProperty("facing");
+						if (_property instanceof DirectionProperty _dp && _dp.getPossibleValues().contains(_dir)) {
+							world.setBlock(_pos, _bs.setValue(_dp, _dir), 3);
+						} else {
+							_property = _bs.getBlock().getStateDefinition().getProperty("axis");
+							if (_property instanceof EnumProperty _ap && _ap.getPossibleValues().contains(_dir.getAxis()))
+								world.setBlock(_pos, _bs.setValue(_ap, _dir.getAxis()), 3);
+						}
 					}
-				}
-			}
-		} else {
-			if (0 < zz - z) {
-				{
-					Direction _dir = Direction.EAST;
-					BlockPos _pos = BlockPos.containing(x, y, z);
-					BlockState _bs = world.getBlockState(_pos);
-					Property<?> _property = _bs.getBlock().getStateDefinition().getProperty("facing");
-					if (_property instanceof DirectionProperty _dp && _dp.getPossibleValues().contains(_dir)) {
-						world.setBlock(_pos, _bs.setValue(_dp, _dir), 3);
-					} else {
-						_property = _bs.getBlock().getStateDefinition().getProperty("axis");
-						if (_property instanceof EnumProperty _ap && _ap.getPossibleValues().contains(_dir.getAxis()))
-							world.setBlock(_pos, _bs.setValue(_ap, _dir.getAxis()), 3);
-					}
-				}
-			} else {
-				{
-					Direction _dir = Direction.WEST;
-					BlockPos _pos = BlockPos.containing(x, y, z);
-					BlockState _bs = world.getBlockState(_pos);
-					Property<?> _property = _bs.getBlock().getStateDefinition().getProperty("facing");
-					if (_property instanceof DirectionProperty _dp && _dp.getPossibleValues().contains(_dir)) {
-						world.setBlock(_pos, _bs.setValue(_dp, _dir), 3);
-					} else {
-						_property = _bs.getBlock().getStateDefinition().getProperty("axis");
-						if (_property instanceof EnumProperty _ap && _ap.getPossibleValues().contains(_dir.getAxis()))
-							world.setBlock(_pos, _bs.setValue(_ap, _dir.getAxis()), 3);
+				} else {
+					{
+						Direction _dir = Direction.SOUTH;
+						BlockPos _pos = BlockPos.containing(x, y, z);
+						BlockState _bs = world.getBlockState(_pos);
+						Property<?> _property = _bs.getBlock().getStateDefinition().getProperty("facing");
+						if (_property instanceof DirectionProperty _dp && _dp.getPossibleValues().contains(_dir)) {
+							world.setBlock(_pos, _bs.setValue(_dp, _dir), 3);
+						} else {
+							_property = _bs.getBlock().getStateDefinition().getProperty("axis");
+							if (_property instanceof EnumProperty _ap && _ap.getPossibleValues().contains(_dir.getAxis()))
+								world.setBlock(_pos, _bs.setValue(_ap, _dir.getAxis()), 3);
+						}
 					}
 				}
 			}
